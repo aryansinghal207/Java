a559f89b40d39e23bcb7fd9fae1f190829b2572b
Aryan <aryansinghal207@gmail.com>
2024-06-14 16:20:40 +0530

    G

--- intro.java
+++ intro.java
@@ -81,22 +81,39 @@
 
 
 // #Hollow Rectangle Pattern
+// import java.util.*;
+// public class intro{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         for(int i=1;i<=a;i++){
+//             for(int j=1;j<=a+1;j++){
+//                 if(i==1||j==1||i==a||j==a+1){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// #Half Pyramid 
 import java.util.*;
 public class intro{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         for(int i=1;i<=a;i++){
-            for(int j=1;j<=a+1;j++){
-                if(i==1||j==1||i==a||j==a+1){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+
 
