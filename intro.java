b103f151873a28b8794af5cf3b73a13862709e49
Aryan <aryansinghal207@gmail.com>
2024-06-14 10:38:54 +0530

    G

--- intro.java
+++ intro.java
@@ -3,6 +3,8 @@ public class intro{
         int a=10;
         int b=20;
         int c=a+b;
-        System.out.println(c);  
+        int d=a*b;
+        System.out.println(c);
+        System.out.println(d);  
     }
 }
