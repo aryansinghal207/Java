e0885586fd20aa1704a05cb5a08856a73cfde9f8
Aryan <aryansinghal207@gmail.com>
2024-06-14 15:36:16 +0530

    g

--- intro.java
+++ intro.java
@@ -64,6 +64,23 @@
 // }
 
 
+// #Rectangle Pattern
+// import java.util.*;
+// public class intro{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         for(int i=1;i<=a;i++){
+//             for(int j=1;j<=a+1;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// #Hollow Rectangle Pattern
 import java.util.*;
 public class intro{
     public static void main(String[] args){
@@ -71,9 +88,15 @@ public class intro{
         int a=sc.nextInt();
         for(int i=1;i<=a;i++){
             for(int j=1;j<=a+1;j++){
-                System.out.print("*");
+                if(i==1||j==1||i==a||j==a+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
 }
+
