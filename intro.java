7f224d4e692b559f1daf13cdbb99bd8998075546
Aryan <aryansinghal207@gmail.com>
2024-06-14 17:45:55 +0530

    G

--- intro.java
+++ intro.java
@@ -134,20 +134,36 @@
 
 
 // #Inverted Half Pyramid
+// import java.util.*;
+// public class intro{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         for(int i=1;i<=a;i++){
+//             for(int j=1;j<=a-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+
+//             System.out.println();
+//         }  
+//     }
+// }
+
+
+// #half Pyramid With Numbers
 import java.util.*;
 public class intro{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         for(int i=1;i<=a;i++){
-            for(int j=1;j<=a-i;j++){
-                System.out.print(" ");
-            }
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(j+" ");
             }
-
             System.out.println();
-        }  
+        }
     }
 }
