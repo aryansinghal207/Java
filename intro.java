0645e5542d493cb34820e28fdff11fb1d4e0a6b7
Aryan <aryansinghal207@gmail.com>
2024-06-14 11:13:07 +0530

    g

--- intro.java
+++ intro.java
@@ -1,10 +1,30 @@
+// public class intro{
+//     public static void main(String[] args){
+//         int a=10;
+//         int b=20;
+//         int c=a+b;
+//         int d=a*b;
+//         System.out.println(c);
+//         System.out.println(d);  
+//     }
+// }
+
+
+// import java.util.*;
+// public class intro{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         String name= sc.nextLine();
+//         System.out.println(name); 
+//     }
+// }
+
+import java.util.*;
 public class intro{
     public static void main(String[] args){
-        int a=10;
-        int b=20;
-        int c=a+b;
-        int d=a*b;
-        System.out.println(c);
-        System.out.println(d);  
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(2*3.14*a); 
     }
 }
+
