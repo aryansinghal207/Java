59bc28bcd299834cfe44833fe9556b4df888a7ec
Aryan <aryansinghal207@gmail.com>
2024-06-14 16:38:59 +0530

    g

--- intro.java
+++ intro.java
@@ -108,7 +108,7 @@
 //         Scanner sc=new Scanner(System.in);
 //         int a=sc.nextInt();
 //         for(int i=1;i<=a;i++){
-//             for(int j=0;j<=i;j++){
+//             for(int j=1;j<=i;j++){
 //                 System.out.print("*");
 //             }
 //             System.out.println();
@@ -140,10 +140,14 @@ public class intro{
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         for(int i=1;i<=a;i++){
+            for(int j=1;j<=a-i;j++){
+                System.out.print(" ");
+            }
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
+
             System.out.println();
-        }
+        }  
     }
 }
