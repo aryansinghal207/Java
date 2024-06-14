6447484c1fd7eccce9f942fb2382f6e367fec380
Aryan <aryansinghal207@gmail.com>
2024-06-14 12:25:26 +0530

    g

--- intro.java
+++ intro.java
@@ -32,14 +32,33 @@
 // }
 
 
-// Print the table of number entered by user
+// Print the table of number entered by user using loop
+// import java.util.*;
+// public class intro{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a=sc.nextInt();
+//         for(int i=1;i<=10;i++){
+//             System.out.println(a*i);
+//         }
+//     }
+// }
+
+
+// #Using Switch in conditional statement
 import java.util.*;
 public class intro{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
-        for(int i=1;i<=10;i++){
-            System.out.println(a*i);
+        switch(a){
+            case 1: System.out.println("Hello");
+            break;
+            case 2: System.out.println("Namaste");
+            break;
+            case 3: System.out.println("Bonjour");
+            break;
+            default : System.out.println("Invalid Button");
         }
     }
 }
