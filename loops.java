f56362e99ffb01f38ffc0900fcb7c28408d31508
Aryan <aryansinghal207@gmail.com>
2024-06-16 14:18:06 +0530

    g

--- loops.java
+++ loops.java
@@ -18,13 +18,13 @@
 
 
 // Print the table of number entered by user using loop
-// import java.util.*;
-// public class intro{
-//     public static void main(String[] args){
-//         Scanner sc = new Scanner(System.in);
-//         int a=sc.nextInt();
-//         for(int i=1;i<=10;i++){
-//             System.out.println(a*i);
-//         }
-//     }
-// }
+import java.util.*;
+public class loops{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(a*i);
+        }
+    }
+}
