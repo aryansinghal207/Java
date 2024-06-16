b374949093b8653aae2380115c5de5641b7f3585
Aryan <aryansinghal207@gmail.com>
2024-06-16 14:29:48 +0530

    Java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,17 +1,35 @@
 import java.util.*;
-public class intro{
+public class pattern{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         for(int i=1;i<=a;i++){
             for(int j=1;j<=i;j++){
-                int sum=i+j;
-                if(sum%2==0){
-                    System.out.print("1");
-                }
-                else{
-                    System.out.print("0");
-                }
+                System.out.print("*");
+            }
+
+            int spaces=2*(a-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=a;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            int spaces=2*(a-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
