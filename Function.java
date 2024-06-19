74309a8bddd63fe992c33740d9dd8a8691faf39c
Aryan <aryansinghal207@gmail.com>
2024-06-19 15:12:16 +0530

    g

--- Function.java
+++ Function.java
@@ -38,23 +38,45 @@
 
 
 // #To print the sum of odd number from 1 to n
+// import java.util.*;
+// public class Function{
+//     public static int sumOfOddNumbers(int n) {
+//         int sum = 0;
+//         for (int i = 1; i <= n; i++) {
+//             if (i % 2 != 0) {
+//                 sum += i;
+//             }
+//         }
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter a positive integer n: ");
+//         int n = scanner.nextInt();
+//         int result = sumOfOddNumbers(n);
+//         System.out.println("The sum of odd numbers from 1 to " + n + " is: " + result);
+//     }
+// }
+
+
+// #Write a function which takes in 2 numbers and returns the greater of those two.
 import java.util.*;
 public class Function{
-    public static int sumOfOddNumbers(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 != 0) {
-                sum += i;
-            }
+    public static int findGreater(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
         }
-        return sum;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a positive integer n: ");
-        int n = scanner.nextInt();
-        int result = sumOfOddNumbers(n);
-        System.out.println("The sum of odd numbers from 1 to " + n + " is: " + result);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        int greater = findGreater(num1, num2);
+        System.out.println("The greater number between " + num1 + " and " + num2 + " is: " + greater);
     }
 }
