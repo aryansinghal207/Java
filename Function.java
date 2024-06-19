92b0b23ee093066b677c36a018dba77ee2831a5d
Aryan <aryansinghal207@gmail.com>
2024-06-19 13:23:15 +0530

    g

--- Function.java
+++ Function.java
@@ -15,23 +15,46 @@
 
 
 // #Add 2 number using functions
+// import java.util.*;
+// public class Function{
+//     public static void factorail(int a){
+//         if(a<0){
+//             System.out.println("Invalid Number");
+//             return;
+//         }
+//         int fact=1;
+//         for(int i=a;i>=1;i--){
+//             fact=fact*i;
+//         }
+//         System.out.print(fact);
+//         return;
+//     }
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         factorail(a);
+//     }
+// }
+
+
+// #To print the sum of odd number from 1 to n
 import java.util.*;
 public class Function{
-    public static void factorail(int a){
-        if(a<0){
-            System.out.println("Invalid Number");
-            return;
+    public static int sumOfOddNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
         }
-        int fact=1;
-        for(int i=a;i>=1;i--){
-            fact=fact*i;
-        }
-        System.out.print(fact);
-        return;
+        return sum;
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        factorail(a);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer n: ");
+        int n = scanner.nextInt();
+        int result = sumOfOddNumbers(n);
+        System.out.println("The sum of odd numbers from 1 to " + n + " is: " + result);
     }
 }
