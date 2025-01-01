8ecb194ef14ed3a1e94e633dad75c4477f6a793d
Aryan <aryansinghal207@gmail.com>
2025-01-01 06:39:34 +0530

    Java

--- Arraylist.java
+++ Arraylist.java
@@ -3,16 +3,24 @@
 import java.util.*;
 public class Arraylist{
     public static void main(String[] args){
-        Integer i=Integer.valueOf(4);
-        System.out.println(i);
-        Float f=Float.valueOf(3.3f);
-        System.out.println(f);
+        // Integer in=Integer.valueOf(4);
+        // System.out.println(in);
+        // Float f=Float.valueOf(3.3f);
+        // System.out.println(f);
         ArrayList<Integer> l1=new ArrayList<>();
         // Arraylist<Boolean> l2=new Arraylist<>();
         l1.add(5);
         l1.add(6);
         l1.add(7);
         l1.add(8);
-        System.out.println(l1.get(1));
+        // System.out.println(l1.get(1));
+        // for(int i=0;i<l1.size();i++){
+        //     System.out.print(l1.get(i)+" ");
+        // }
+        // System.out.println();
+        System.out.println(l1);
+        l1.add(1,100);
+        System.out.println(l1);
+
     }
 }
