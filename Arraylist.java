3f15eb717351747b07b2e5a3da57f8885dbbe621
Aryan <aryansinghal207@gmail.com>
2025-01-01 11:55:30 +0530

    Java

--- Arraylist.java
+++ Arraylist.java
@@ -31,38 +31,55 @@
 //     }
 // }
 
+// // WAP to reverse a Arraylist
+// import java.util.ArrayList;
+// import java.util.Collections;
+// public class Arraylist{
+//     // static void reverselist(ArrayList<Integer> list){
+//     //     int i=0,j=list.size()-1;
+//     //     while(i<j){
+//     //         Integer temp=Integer.valueOf(list.get(i));
+//     //         list.set(i,list.get(j));
+//     //         list.set(j,temp);
+//     //         i++;
+//     //         j--;
+//     //     }
+//     // }
+//     public static void main(String[] args){
+//         ArrayList<Integer> l1=new ArrayList<>();
+//         l1.add(5);
+//         l1.add(6);
+//         l1.add(7);
+//         l1.add(8);
+//         l1.add(1);
+//         l1.add(2);
+//         l1.add(3);
+//         l1.add(4);
+//         System.out.println("Original List : "+l1);
+//         // reverselist(l1);
+//         // We could use predefines library of collection in which we have predefined reverse function
+//         Collections.reverse(l1);
+//         System.out.println("Reversed List : "+l1);
+//         Collections.sort(l1);
+//         System.out.println("Reversed List : "+l1);
+//         Collections.sort(l1,Collections.reverseOrder());
+//         System.out.println("Reversed List : "+l1);
+//     }
+// }
+
+
 // WAP to reverse a Arraylist
 import java.util.ArrayList;
 import java.util.Collections;
 public class Arraylist{
-    // static void reverselist(ArrayList<Integer> list){
-    //     int i=0,j=list.size()-1;
-    //     while(i<j){
-    //         Integer temp=Integer.valueOf(list.get(i));
-    //         list.set(i,list.get(j));
-    //         list.set(j,temp);
-    //         i++;
-    //         j--;
-    //     }
-    // }
     public static void main(String[] args){
-        ArrayList<Integer> l1=new ArrayList<>();
-        l1.add(5);
-        l1.add(6);
-        l1.add(7);
-        l1.add(8);
-        l1.add(1);
-        l1.add(2);
-        l1.add(3);
-        l1.add(4);
-        System.out.println("Original List : "+l1);
-        // reverselist(l1);
-        // We could use predefines library of collection in which we have predefined reverse function
-        Collections.reverse(l1);
-        System.out.println("Reversed List : "+l1);
+        ArrayList<String> l1=new ArrayList<>();
+        l1.add("Welcome");
+        l1.add("To");
+        l1.add("Physics");
+        l1.add("Wallah");
+        System.out.println("Original Order : "+l1);
         Collections.sort(l1);
-        System.out.println("Reversed List : "+l1);
-        Collections.sort(l1,Collections.reverseOrder());
-        System.out.println("Reversed List : "+l1);
+        System.out.println("Reversed Order : "+l1);
     }
 }
