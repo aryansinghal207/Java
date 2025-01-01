a17513a6de1a68d2d68ebf9ff43f8eab5996a0e6
Aryan <aryansinghal207@gmail.com>
2025-01-01 06:24:26 +0530

    Java

---
+++ Methods.java
@@ -0,0 +1,36 @@
+// import java.util.Scanner;
+// class Algebra{
+//     int add(int a,int b){
+//         int ans=a+b;
+//         return ans;
+//     }
+// }
+// public class Methods{
+//     public static void main(String[] args){
+//         Algebra obj=new Algebra();
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         int ans=obj.add(a,b);
+//         System.out.println(ans);
+//     }
+// }
+
+// // User Defined method
+// public class Methods{
+//     public static void main(String[] args){
+//         System.out.println(Math.sqrt(24));
+//         System.out.println(Math.sqrt(16));
+
+//         System.out.println(Math.floor(24.9));
+//         System.out.println(Math.floor(19.1));
+
+//         System.out.println(Math.ceil(24.9));
+//         System.out.println(Math.ceil(9.1));
+//     }
+// }
+
+// Package are the predefined keywords like java.util.* etc
+
+// Constructor : Special method used to initialize object
+// Access Modifiers : 4 types :- 1) Public 2) Private 3) Protected 4) Default
