b3efde2e04816562ab0ecaaa299f6ea0e559fa65
Aryan <aryansinghal207@gmail.com>
2025-01-02 18:12:08 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -19,20 +19,42 @@
 // }
 
 
-// WAP to print from n to 1.
+// // WAP to print from n to 1.
+// import java.util.*;
+// public class Recursion{
+//     static void print(int n){
+//         if(n==1){
+//             System.out.println(n);
+//             return;
+//         }
+//         System.out.println(n);
+//         print(n-1);    
+//     }
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         print(n);
+//     }
+// }
+
+
+// WAP to print factorail of any number.
 import java.util.*;
 public class Recursion{
-    static void print(int n){
-        if(n==1){
-            System.out.println(n);
-            return;
+    static int fact(int n){
+        if(n<0){
+            return 0;
         }
-        System.out.println(n);
-        print(n-1);    
+        else if(n==0 || n==1){
+            return 1;
+        }
+        int result=n*fact(n-1);
+        return result;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        print(n);
+        int answer=fact(n);
+        System.out.println(answer);
     }
 }
