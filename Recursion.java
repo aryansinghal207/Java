9005baac3103ded28592f3ee9d1b285e0c821e71
Aryan <aryansinghal207@gmail.com>
2025-01-02 09:42:26 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -1,1 +1,18 @@
 // A Function calling itself
+import java.util.*;
+public class Recursion{
+    static void print(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        print(n);
+    }
+}
