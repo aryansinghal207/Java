4cd77837709607226549236bfb2a4d785c45b4b6
Aryan <aryansinghal207@gmail.com>
2025-01-02 16:00:42 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,25 @@
 // A Function calling itself
-// WAP to print from 1 to n.
+// // WAP to print from 1 to n.
+// import java.util.*;
+// public class Recursion{
+//     static void print(int n){
+//         if(n==1){
+//             System.out.println(n);
+//             return;
+//         }
+//         print(n-1);
+//         System.out.println(n);
+    
+//     }
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         print(n);
+//     }
+// }
+
+
+// WAP to print from n to 1.
 import java.util.*;
 public class Recursion{
     static void print(int n){
@@ -7,9 +27,8 @@ public class Recursion{
             System.out.println(n);
             return;
         }
-        print(n-1);
         System.out.println(n);
-    
+        print(n-1);    
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
