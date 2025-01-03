3086847c017120468bf37d8fa0d68b2391f5f9e3
Aryan <aryansinghal207@gmail.com>
2025-01-03 19:59:23 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -161,3 +161,25 @@
 //         System.out.print(ans);
 //     }
 // }
+
+
+//Find GCD 
+// Better then Brute Force Approach
+import java.util.*;
+public class Recursion{
+    public static int gcd(int x,int y){
+        while(x%y!=0){
+            int rem=x%y;
+            x=y;
+            y=rem;
+        }
+        return y;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int ans=gcd(x,y);
+        System.out.print(ans);
+    }
+}
