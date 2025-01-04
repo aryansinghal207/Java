bb15d8b322c9f7f3513331ebf784b85e73c93e35
Aryan <aryansinghal207@gmail.com>
2025-01-04 05:58:59 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -184,20 +184,21 @@
 //     }
 // }
 
-// Euclid's Algorithum
-import java.util.*;
-public class Recursion{
-    public static int gcd(int x,int y){
-        if(y==0){
-            return x; 
-        }
-        return gcd(y,x%y);
-    }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        int ans=gcd(x,y);
-        System.out.print(ans);
-    }
-}
+// // Euclid's Algorithum
+// import java.util.*;
+// public class Recursion{
+//     public static int gcd(int x,int y){
+//         if(y==0){
+//             return x; 
+//         }
+//         return gcd(y,x%y);
+//     }
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int x=sc.nextInt();
+//         int y=sc.nextInt();
+//         int ans=gcd(x,y);
+//         System.out.print(ans);
+//     }
+// }
+
