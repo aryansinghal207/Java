5ec77a706ef98fe3a48a044edb222dd19b101456
Aryan <aryansinghal207@gmail.com>
2025-01-04 09:57:39 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -202,3 +202,19 @@
 //     }
 // }
 
+
+// WAP to print all the value array recursively.
+import java.util.*;
+public class Recursion{
+    public static void printArray(int[] x,int y){
+        if(y==x.length){
+            return;
+        }
+        System.out.print(x[y]+" ");
+        printArray(x,y+1);
+    }
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5};
+        printArray(arr,0);
+    }
+}
