d01aa85c840669c70c68c90e733528362426bb58
Aryan <aryansinghal207@gmail.com>
2025-01-04 19:27:56 +0530

    Java

--- Recursion2.java
+++ Recursion2.java
@@ -1,24 +1,24 @@
 // WAP to print weather x exist in an array or not.
 import java.util.Scanner;
 public class Recursion2 {
-    static boolean findnum(int[] arr, int x, int n, int index) {
-        if (index >= n) {
+    static boolean findnum(int[] arr, int target, int size, int index) {
+        if (index >= size) {
             return false;
         }
-        if (arr[index] == x) {
+        if (arr[index] == target) {
             return true;
         }
-        return findnum(arr, x, n, index + 1);
+        return findnum(arr, target, size, index + 1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        int x = sc.nextInt();
-        if (findnum(arr, x, n, 0)) {
+        int target = sc.nextInt();
+        if (findnum(arr, target, size, 0)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
