14265af42165b8b97cd3afc82eaac7e7512c0be4
Aryan <aryansinghal207@gmail.com>
2025-01-04 19:36:17 +0530

    Java

--- Recursion2.java
+++ Recursion2.java
@@ -1,12 +1,41 @@
-// WAP to print weather x exist in an array or not.
+// // WAP to print weather x exist in an array or not using true or false.
+// import java.util.Scanner;
+// public class Recursion2 {
+//     static boolean findnum(int[] arr, int target, int size, int index) {
+//         if (index >= size) {
+//             return false;
+//         }
+//         if (arr[index] == target) {
+//             return true;
+//         }
+//         return findnum(arr, target, size, index + 1);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int[] arr = new int[size];
+//         for (int i = 0; i < size; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         int target = sc.nextInt();
+//         if (findnum(arr, target, size, 0)) {
+//             System.out.println("Yes");
+//         } else {
+//             System.out.println("No");
+//         }
+//     }
+// }
+
+
+// WAP to print weather x exist in an array if yes the return index of that otherwise return -1.
 import java.util.Scanner;
 public class Recursion2 {
-    static boolean findnum(int[] arr, int target, int size, int index) {
+    static int findnum(int[] arr, int target, int size, int index) {
         if (index >= size) {
-            return false;
+            return -1;
         }
         if (arr[index] == target) {
-            return true;
+            return index;
         }
         return findnum(arr, target, size, index + 1);
     }
@@ -18,10 +47,7 @@ public class Recursion2 {
             arr[i] = sc.nextInt();
         }
         int target = sc.nextInt();
-        if (findnum(arr, target, size, 0)) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        int result=findnum(arr, target, size, 0);
+        System.out.print(result);
     }
 }
