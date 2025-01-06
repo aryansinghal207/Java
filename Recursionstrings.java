24dcdfa88ee24f1cf7108b2e0f77870ae811023c
Aryan <aryansinghal207@gmail.com>
2025-01-06 14:00:08 +0530

    Java

--- Recursionstrings.java
+++ Recursionstrings.java
@@ -77,26 +77,52 @@
 //     }
 // }
 // // or
+// import java.util.*;
+// public class Recursionstrings {
+//     public static boolean isPalindrome(char[] str, int start, int end) {
+//         if (start >= end) {
+//             return true;
+//         }
+//         if (str[start] != str[end]) {
+//             return false;
+//         }
+//         return isPalindrome(str, start + 1, end - 1);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String input=sc.nextLine();
+//         char[] str = input.toCharArray();
+//         boolean result = isPalindrome(str, 0, str.length - 1);
+//         if (result) {
+//             System.out.println("The string \"" + input + "\" is a palindrome.");
+//         } else {
+//             System.out.println("The string \"" + input + "\" is not a palindrome.");
+//         }
+//     }
+// }
+
+
+// WAP to print the substring of a String
 import java.util.*;
 public class Recursionstrings {
-    public static boolean isPalindrome(char[] str, int start, int end) {
-        if (start >= end) {
-            return true;
+    public static ArrayList<String> getsubstring(String s){
+        ArrayList<String> ans=new ArrayList<>();
+        if(s.length()==0){
+            ans.add("");
+            return ans;
         }
-        if (str[start] != str[end]) {
-            return false;
+        char curr=s.charAt(0);
+        ArrayList<String> smallAns=getsubstring(s.substring(1));
+        for(String ss:smallAns){
+            ans.add(ss);
+            ans.add(curr + ss);
         }
-        return isPalindrome(str, start + 1, end - 1);
+        return ans;
     }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String input=sc.nextLine();
-        char[] str = input.toCharArray();
-        boolean result = isPalindrome(str, 0, str.length - 1);
-        if (result) {
-            System.out.println("The string \"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is not a palindrome.");
+        ArrayList<String> ans=getsubstring("abc");
+        for(String ss: ans){
+            System.out.println(ss);
         }
     }
 }
