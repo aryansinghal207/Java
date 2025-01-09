f27f195898dbe2c36eaaece57a0ebd15761eae41
Aryan <aryansinghal207@gmail.com>
2025-01-09 23:55:54 +0530

    Java

---
+++ Bubblesort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Bubblesort{
+    static void sort(int[] a,int n) {
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr,size);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
