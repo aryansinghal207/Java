3d174098574bddabc87601e341e9cbe04a9fa7b0
Aryan <aryansinghal207@gmail.com>
2025-01-10 00:08:22 +0530

    Java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,13 +1,47 @@
+// import java.util.Scanner;
+// public class Bubblesort{
+//     static void sort(int[] a,int n) {
+//         for(int i=0;i<n-1;i++){
+//             for(int j=0;j<n-i-1;j++){
+//                 if(a[j]>a[j+1]){
+//                     int temp=a[j];
+//                     a[j]=a[j+1];
+//                     a[j+1]=temp;
+//                 }
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         int size=sc.nextInt();
+//         int[] arr = new int[size];
+//         for (int i = 0; i < size; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         sort(arr,size);
+//         for (int i : arr) {
+//             System.out.print(i+" ");
+//         }
+//     }
+// }
+
+
+// Optimal Approach of the previous code 
 import java.util.Scanner;
 public class Bubblesort{
     static void sort(int[] a,int n) {
         for(int i=0;i<n-1;i++){
+            boolean flag=false;
             for(int j=0;j<n-i-1;j++){
                 if(a[j]>a[j+1]){
                     int temp=a[j];
                     a[j]=a[j+1];
                     a[j+1]=temp;
+                    flag=true;
                 }
+            }
+            if(flag==false){
+                return;
             }
         }
     }
