d189768dbcfce0f2f8b7cb3f9ec9c03eba8bf4b5
Aryan <aryansinghal207@gmail.com>
2025-01-10 00:42:14 +0530

    Java

---
+++ Selectionsort.java
@@ -0,0 +1,30 @@
+// 
+
+import java.util.Scanner;
+public class Selectionsort{
+    static void sort(int[] a,int n) {
+        for(int i=0;i<n-1;i++){
+            int min_index=i;
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[min_index]){
+                    min_index=j;
+                }
+            }
+            int temp=a[i];
+            a[i]=a[min_index];
+            a[min_index]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr,size);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
