18676cbc186ee525ac3da6d637381690de52d481
Aryan <aryansinghal207@gmail.com>
2025-01-10 12:14:57 +0530

    Java

---
+++ Insertionsort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Insertionsort{
+    static void sort(int[] a,int n) {
+        for(int i=1;i<n;i++){
+            int j=i;
+            while(j>0 && a[j]<a[j-1]){
+                int temp=a[j];
+                a[j]=a[j-1];
+                a[j-1]=temp;
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr,size);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
