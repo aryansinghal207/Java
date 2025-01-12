71bd855c323420b39ee876bee4758cc215bd4965
Aryan <aryansinghal207@gmail.com>
2025-01-12 21:56:08 +0530

    Java

--- Quicksort.java
+++ Quicksort.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Quicksort{ 
-    
+    static void sort(int[] arr){
+        
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int size=sc.nextInt();
