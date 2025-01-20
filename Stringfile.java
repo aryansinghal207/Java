cc75325faf087549738fa0239537e0551ad5d7a8
Aryan <aryansinghal207@gmail.com>
2025-01-20 23:47:21 +0530

    Java

--- Stringfile.java
+++ Stringfile.java
@@ -23,7 +23,7 @@ import java.util.*;
 public class Stringfile{ 
     public static void printletter(String str){
         for(int i=0;i<str.length();i++){
-            System.out.print(str.charAt(i));
+            System.out.print(str.charAt(i)+" ");
         }
     }
     public static void main(String[] args) {
