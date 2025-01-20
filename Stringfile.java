a148764d6bdc13aa90e7cde8f78fef228036a53d
Aryan <aryansinghal207@gmail.com>
2025-01-20 23:45:14 +0530

    Java

--- Stringfile.java
+++ Stringfile.java
@@ -1,14 +1,21 @@
 // Strings are IMMUTABLE in java
-import java.util.*;
-public class Stringfile{ 
-    public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // String name; 
-        // name = sc.nextLine();
-        // System.out.println(name);
-        String firstname="Aryan";
-        String lastname="Singhal";
-        String fullname=firstname+" "+lastname;
-        System.out.println(fullname);
-    }
-}
+// import java.util.*;
+// public class Stringfile{ 
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String name; 
+//         name = sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
+
+// import java.util.*;
+// public class Stringfile{ 
+//     public static void main(String[] args) {
+//         String firstname="Aryan";
+//         String lastname="Singhal";
+//         String fullname=firstname+" "+lastname;
+//         System.out.println(fullname);
+//     }
+// }
+
