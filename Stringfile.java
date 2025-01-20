8c7f5a4ce5b35175bc91a70dfbc274900c886e91
Aryan <aryansinghal207@gmail.com>
2025-01-20 23:47:11 +0530

    Java

--- Stringfile.java
+++ Stringfile.java
@@ -19,3 +19,17 @@
 //     }
 // }
 
+import java.util.*;
+public class Stringfile{ 
+    public static void printletter(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i));
+        }
+    }
+    public static void main(String[] args) {
+        String firstname="Aryan";
+        String lastname="Singhal";
+        String fullname=firstname+" "+lastname;
+        printletter(fullname);
+    }
+}
