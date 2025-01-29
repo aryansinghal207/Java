49ba9b4bf6d3ab8cfe4d446b27e63455ac62410e
Aryan <aryansinghal207@gmail.com>
2025-01-29 16:27:07 +0530

    Java

--- OOPS.java
+++ OOPS.java
@@ -32,3 +32,30 @@
 //     }
 // }
 
+public class OOPS{
+    public static void main(String args[]){
+        Horse h=new Horse();
+        h.eat();
+        h.walk();
+
+        Chicken c=new Chicken();
+        c.eat();
+        c.walk();
+    }
+}
+abstract class Animal{
+    void eat(){
+        System.out.println("Animal Eats");
+    }
+    abstract void walk();
+}
+class Horse extends Animal{
+    void walk(){
+        System.out.println("Walks on 4 legs"); 
+    }
+}
+class Chicken extends Animal{
+    void walk(){
+        System.out.println("Walks on 2 legs");
+    }
+}
