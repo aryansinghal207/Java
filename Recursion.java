5c91ec9737a776c3370f2a977635f59b2ccf38db
Aryan <aryansinghal207@gmail.com>
2025-02-01 23:39:35 +0530

    Java

--- Recursion.java
+++ Recursion.java
@@ -300,10 +300,16 @@ class Recursion{
       } 
     return n*factorail(n-1); 
     }
+    public static int sum(int n){
+      if(n==1){
+        return 1;
+      }
+      return n+sum(n-1);
+    }
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       int a = sc.nextInt();
-      int ans=factorail(a);
+      int ans=sum(a);
       System.out.println(ans);
     }
 }
