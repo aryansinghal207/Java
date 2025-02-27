8869a783f77ad53e66d6e8f58bdb3534bff770d8
Aryan <aryansinghal207@gmail.com>
2025-02-27 17:44:34 +0530

    Java

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,3 +1,116 @@
+// import java.util.*;
+// public class Linkedlist{
+//     public static class Node{
+//         int data;
+//         Node next;
+//         public Node(int data){
+//             this.data=data;
+//             this.next=null;
+//         }
+//     }
+//     public static Node head;
+//     public static Node tail;
+//     public static int size;
+//     public void addfirst(int data){
+//         Node newNode=new Node(data);
+//         size++;
+//         if(head==null){
+//             head=tail=newNode;
+//             return;
+//         }
+//         newNode.next=head;
+//         head=newNode;
+//     }
+//     public void addlast(int data){
+//         Node newNode=new Node(data);
+//         size++;
+//         if(head==null){
+//             head=tail=newNode;
+//             return;
+//         }
+//         tail.next=newNode;
+//         tail=newNode;
+//     }
+//     public void print(){
+//         if(head==null){
+//             System.out.print("Linked list is empty");
+//         }
+//         Node temp=head;   // We have used temp for that we could iterate on every step without changing head and tail bcz head and tail position are fixed and only changes when change in linked list.
+//         while(temp != null){
+//             System.out.print(temp.data+" ");
+//             temp=temp.next;
+//         }
+//         System.out.println();
+//     }
+//     public void add(int idx,int data){
+//         if(idx==0){
+//             addfirst(data);
+//             return;
+//         }
+//         Node newNode=new Node(data);
+//         size++;
+//         Node temp=head;
+//         int i=0;
+//         while(i<idx-1){
+//             temp=temp.next;
+//             i++;
+//         }
+//         newNode.next=temp.next;
+//         temp.next=newNode;
+//     }
+//     public int removefirst(){
+//         if(size==0){
+//             System.out.print("Linkedlist is empty");
+//             return Integer.MIN_VALUE;
+//         }else if(size==1){
+//             int val=head.data;
+//             head=tail=null;
+//             return val;
+//         }
+//         int val=head.data;
+//         head=head.next;
+//         size--;
+//         return val;
+//     }
+//     public int removelast(){
+//         if(size==0){
+//             System.out.print("Linkedlist is empty");
+//             return Integer.MIN_VALUE;
+//         }else if(size==1){
+//             int val=head.data;
+//             head=tail=null;
+//             size=0;
+//             return val;
+//         }
+//         Node prev=head;
+//         for(int i=0;i<size-2;i++){
+//             prev=prev.next; 
+//         }
+//         int val=prev.next.data;
+//         prev.next=null;
+//         tail=prev;
+//         size--;
+//         return val;
+//     }
+//     public static void main(String args[]){
+//         Linkedlist l1=new Linkedlist();
+//         l1.addfirst(2);
+//         l1.addfirst(1);
+//         l1.addlast(3);
+//         l1.addlast(4);
+//         l1.add(2,9);
+//         l1.print();
+//         System.out.println(Linkedlist.size);
+//         l1.removefirst();
+//         l1.print();
+//         System.out.println(Linkedlist.size);
+//         l1.removelast();
+//         l1.print();
+//         System.out.println(Linkedlist.size);
+//     }
+// }
+
+
 import java.util.*;
 public class Linkedlist{
     public static class Node{
@@ -31,46 +144,17 @@ public class Linkedlist{
         tail.next=newNode;
         tail=newNode;
     }
-    public void print(){
-        if(head==null){
-            System.out.print("Linked list is empty");
-        }
-        Node temp=head;   // We have used temp for that we could iterate on every step without changing head and tail bcz head and tail position are fixed and only changes when change in linked list.
-        while(temp != null){
-            System.out.print(temp.data+" ");
-            temp=temp.next;
-        }
-        System.out.println();
-    }
-    public void add(int idx,int data){
-        if(idx==0){
-            addfirst(data);
-            return;
-        }
-        Node newNode=new Node(data);
-        size++;
+    public int itrsearch(int key){
         Node temp=head;
         int i=0;
-        while(i<idx-1){
+        while(temp!=null){
+            if(temp.data==key){
+                return i;
+            }
             temp=temp.next;
             i++;
         }
-        newNode.next=temp.next;
-        temp.next=newNode;
-    }
-    public int removefirst(){
-        if(size==0){
-            System.out.print("Linkedlist is empty");
-            return Integer.MIN_VALUE;
-        }else if(size==1){
-            int val=head.data;
-            head=tail=null;
-            return val;
-        }
-        int val=head.data;
-        head=head.next;
-        size--;
-        return val;
+        return -1;
     }
     public static void main(String args[]){
         Linkedlist l1=new Linkedlist();
@@ -78,11 +162,7 @@ public class Linkedlist{
         l1.addfirst(1);
         l1.addlast(3);
         l1.addlast(4);
-        l1.add(2,9);
-        l1.print();
-        System.out.println(Linkedlist.size);
-        l1.removefirst();
-        l1.print();
-        System.out.println(Linkedlist.size);
+        System.out.println(l1.itrsearch(3));
+        System.out.println(l1.itrsearch(10));
     }
 }
--- Quicksort.java
+++ Quicksort.java
@@ -1,86 +1,86 @@
-// import java.util.Scanner;
-// public class Quicksort { 
-//     static void swap(int[] arr, int x, int y) {
-//         int temp = arr[x];
-//         arr[x] = arr[y];
-//         arr[y] = temp;
-//     }
-//     static int partition(int[] arr, int st, int end) {
-//         int pivot = arr[st];
-//         int cnt = 0;
-//         for (int i = st + 1; i <= end; i++) {
-//             if (arr[i] < pivot) cnt++;
-//         }
-//         int pivotidx = st + cnt;
-//         swap(arr, st, pivotidx);
-//         int i = st, j = end;
-//         while (i < pivotidx && j > pivotidx) {
-//             while (arr[i] <= pivot && i < pivotidx) i++;
-//             while (arr[j] > pivot && j > pivotidx) j--;
-//             if (i < pivotidx && j > pivotidx) {
-//                 swap(arr, i, j);
-//                 i++;
-//                 j--;
-//             }
-//         }
-//         return pivotidx;
-//     }
-//     static void Quicksort(int[] arr, int st, int end) {
-//         if (st >= end) return; 
-//         int pi = partition(arr, st, end); 
-//         Quicksort(arr, st, pi - 1); 
-//         Quicksort(arr, pi + 1, end); 
-//     }
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int size = sc.nextInt();
-//         int[] arr = new int[size];
-//         for (int i = 0; i < size; i++) {
-//             arr[i] = sc.nextInt();
-//         }
-//         Quicksort(arr, 0, size - 1);
-//         for (int i : arr) {
-//             System.out.print(i + " ");
-//         }
-//     }
-// }
-    
-
-import java.util.*;
-public class Quicksort{
-    public static void printArray(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
+import java.util.Scanner;
+public class Quicksort { 
+    static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
-    public static void Quicksort(int[] arr,int si,int ei){
-        if(si >= ei){
-            return;
+    static int partition(int[] arr, int st, int end) {
+        int pivot = arr[st];
+        int cnt = 0;
+        for (int i = st + 1; i <= end; i++) {
+            if (arr[i] < pivot) cnt++;
         }
-        int pIdx=partition(arr,si,ei);
-        Quicksort(arr,si,pIdx-1);
-        Quicksort(arr,pIdx+1,ei);
-    }
-    public static int partition(int[] arr,int si,int ei){
-        int pivot=arr[ei];
-        int i=si-1;
-        for(int j=si;j<ei;j++){
-            if(arr[j]<=pivot){
+        int pivotidx = st + cnt;
+        swap(arr, st, pivotidx);
+        int i = st, j = end;
+        while (i < pivotidx && j > pivotidx) {
+            while (arr[i] <= pivot && i < pivotidx) i++;
+            while (arr[j] > pivot && j > pivotidx) j--;
+            if (i < pivotidx && j > pivotidx) {
+                swap(arr, i, j);
                 i++;
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
+                j--;
             }
         }
-        i++;
-        int temp=arr[ei];
-        arr[ei]=arr[i];
-        arr[i]=temp;
-        return i;
+        return pivotidx;
     }
-    public static void main(String args[]){
-        int arr[]={2,4,3,1,4,5,7,-1};
-        Quicksort(arr,0,arr.length-1);
-        printArray(arr);
+    static void Quicksort(int[] arr, int st, int end) {
+        if (st >= end) return; 
+        int pi = partition(arr, st, end); 
+        Quicksort(arr, st, pi - 1); 
+        Quicksort(arr, pi + 1, end); 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Quicksort(arr, 0, size - 1);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
+    
+
+// import java.util.*;
+// public class Quicksort{
+//     public static void printArray(int[] arr){
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+//     public static void Quicksort(int[] arr,int si,int ei){
+//         if(si >= ei){
+//             return;
+//         }
+//         int pIdx=partition(arr,si,ei);
+//         Quicksort(arr,si,pIdx-1);
+//         Quicksort(arr,pIdx+1,ei);
+//     }
+//     public static int partition(int[] arr,int si,int ei){
+//         int pivot=arr[ei];
+//         int i=si-1;
+//         for(int j=si;j<ei;j++){
+//             if(arr[j]<=pivot){
+//                 i++;
+//                 int temp=arr[j];
+//                 arr[j]=arr[i];
+//                 arr[i]=temp;
+//             }
+//         }
+//         i++;
+//         int temp=arr[ei];
+//         arr[ei]=arr[i];
+//         arr[i]=temp;
+//         return i;
+//     }
+//     public static void main(String args[]){
+//         int arr[]={2,4,3,1,4,5,7,-1};
+//         Quicksort(arr,0,arr.length-1);
+//         printArray(arr);
+//     }
+// }
