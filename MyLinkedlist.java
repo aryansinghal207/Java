23ac68453f7b6805e6fc2731e9068a6dda2c3f19
Aryan <aryansinghal207@gmail.com>
2025-03-09 13:03:58 +0530

    java

--- MyLinkedlist.java
+++ MyLinkedlist.java
@@ -535,6 +535,40 @@
 // }
 
 
+// import java.util.*;
+// public class MyLinkedlist{
+//     public static class Node{
+//         int data;
+//         Node next;
+//         public Node(int data){
+//             this.data=data;
+//             this.next=null;
+//         }
+//     }
+//     public static Node head;
+//     public static Node tail;
+//     public static boolean isCycle(){   //Floyd's CFA
+//         Node slow=head;
+//         Node fast=head;
+//         while(fast != null && fast.next != null){
+//             slow=slow.next;
+//             fast=fast.next.next;
+//             if(slow==fast){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public static void main(String[] args){
+//         head=new Node(1);
+//         head.next=new Node(2);
+//         head.next.next=new Node(3);
+//         head.next.next.next=head;
+//         System.out.println(isCycle());
+//     }
+// }
+
+
 import java.util.*;
 public class MyLinkedlist{
     public static class Node{
@@ -559,11 +593,38 @@ public class MyLinkedlist{
         }
         return false;
     }
+    public static void removecycle(){
+        Node slow=head;
+        Node fast=head;
+        boolean Cycle=false;
+        while(fast != null && fast.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                Cycle=true;
+                break;
+            }
+        }
+        if(!Cycle){
+            return;
+        }
+        slow=head;
+        Node prev=null;
+        while(slow != fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        prev.next=null;
+    }
     public static void main(String[] args){
         head=new Node(1);
-        head.next=new Node(2);
+        Node temp=new Node(2);
+        head.next=temp;
         head.next.next=new Node(3);
-        head.next.next.next=head;
+        head.next.next.next=temp;
+        System.out.println(isCycle());
+        removecycle();
         System.out.println(isCycle());
     }
-}    
+}
