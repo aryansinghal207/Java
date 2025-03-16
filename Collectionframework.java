addeb506368f6e903ce0737ca76d7f9865d7b8ee
Aryan <aryansinghal207@gmail.com>
2025-03-16 18:53:21 +0530

    java

--- Collectionframework.java
+++ Collectionframework.java
@@ -88,4 +88,79 @@
 // }
 
 
- 
+import java.util.*;
+public class Collectionframework {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addlast(int data) { 
+        Node newnode = new Node(data);
+        if (head == null) {
+            head = tail = newnode;
+            return;
+        }
+        tail.next = newnode;
+        tail = newnode;
+    }
+    public void print() { 
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public void zigzag() {
+        if (head == null || head.next == null) return; 
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+            left = nextL;
+            right = nextR;
+        }
+    }
+    public static void main(String[] args) {
+        Collectionframework l1 = new Collectionframework();
+        l1.addlast(1); 
+        l1.addlast(2);
+        l1.addlast(3);
+        l1.addlast(4);
+        l1.addlast(5);
+        l1.print(); 
+        l1.zigzag();
+        l1.print();
+    }
+}
