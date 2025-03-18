2b1f1d873b09d278df61bc30f193d1dc6ce47c3c
Aryan <aryansinghal207@gmail.com>
2025-03-18 14:44:47 +0530

    java

--- Doublelinkedlist.java
+++ Doublelinkedlist.java
@@ -24,6 +24,23 @@ public class Doublelinkedlist{
         head.prev=newNode;
         head=newNode;
     }
+    public int removefirst(){
+        if(head == null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size--;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        head.prev=null;
+        size--;
+        return val;
+    }
     public void print(){
         Node temp=head;
         while(temp != null){
@@ -41,6 +58,8 @@ public class Doublelinkedlist{
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
+        dll.print();
+        dll.removefirst();
         dll.print();
         System.out.print(dll.size);
     }
