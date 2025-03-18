569d741b0eac13b2f1d004905286b6ee2bbe76c5
Aryan <aryansinghal207@gmail.com>
2025-03-18 12:44:48 +0530

    java

--- Collection_Framework/Arraysclasscode.java
+++ Collection_Framework/Arraysclasscode.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class Arraysclasscode{
     public static void main(String[] args){
-        
+        int[] numbers={1,4,2,6,5,7,8,9};
+        Arrays.sort(numbers);
+        int index=Arrays.binarySearch(numbers,4);
+        System.out.println("The index of element 4 is : "+index);
     }
 }
