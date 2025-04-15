a84c5251f56b24c304f9a6448cd8a244ad2c277d
Aryan <aryansinghal207@gmail.com>
2025-04-16 00:08:40 +0530

    java

--- Stackcode.java
+++ Stackcode.java
@@ -272,26 +272,51 @@
 // }
 
 
-// Push at Bottom of Stack
+// // Push at Bottom of Stack
+// import java.util.*;
+// public class Stackcode{
+//     public static void pushAtbottom(Stack<Integer> s,int data){
+//         if(s.isEmpty()){
+//             s.push(data);
+//             return;
+//         }
+//         int top=s.pop();
+//         pushAtbottom(s,data);
+//         s.push(top);
+//     }
+//     public static void main(String[] args){
+//         Stack<Integer> s=new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+//         pushAtbottom(s,4);
+//         while(!s.isEmpty()){
+//             System.out.println(s.pop());
+//         }
+//     }
+// }
+
+
+// Reverse a string using Stack
 import java.util.*;
 public class Stackcode{
-    public static void pushAtbottom(Stack<Integer> s,int data){
-        if(s.isEmpty()){
-            s.push(data);
-            return;
+    public static String reverseString(String str){
+        Stack<Character> s=new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
         }
-        int top=s.pop();
-        pushAtbottom(s,data);
-        s.push(top);
+        StringBuilder result=new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            result.append(curr);
+        }
+        return result.toString();
     }
     public static void main(String[] args){
-        Stack<Integer> s=new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        pushAtbottom(s,4);
-        while(!s.isEmpty()){
-            System.out.println(s.pop());
-        }
+        String str="aryan";
+        String result=reverseString(str);
+        System.out.println(result);
     }
 }
