f941aba0d821a9ba02812f9f5ae953f9fb28b8d6
Aryan <aryansinghal207@gmail.com>
2025-04-16 10:14:16 +0530

    java

--- Stackcode.java
+++ Stackcode.java
@@ -140,29 +140,26 @@
 // }
 
 
-// // Reverse a string using stack
-// import java.util.*;
-// public class Stackcode {
-//     public static String reverseString(String originalStr) {
-//         Stack<Character> stack = new Stack<>();
-//         int idx = 0;
-//         while (idx < originalStr.length()) {
-//             stack.push(originalStr.charAt(idx));
-//             idx++;
-//         }
-//         StringBuilder result = new StringBuilder();
-//         while (!stack.isEmpty()) {
-//             char curr = stack.pop();
-//             result.append(curr);
-//         }
-        
-//         return result.toString();  
-//     }
-//     public static void main(String[] args) {
-//         String str = "abc";
-//         String result = reverseString(str);
-//         System.out.println(result); 
-//     }
-// }
-
-
+// Reverse a string using Stack
+import java.util.*;
+public class Stackcode{
+    public static String reverseString(String str){
+        Stack<Character> s=new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result=new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args){
+        String str="aryan";
+        String result=reverseString(str);
+        System.out.println(result);
+    }
+}
