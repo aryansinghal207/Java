45ec3d6f20a71fc79ffa3a6f3f8ccb2e7ac82839
Aryan <aryansinghal207@gmail.com>
2025-04-21 16:47:43 +0530

    java

--- PriorityQueuecode.java
+++ PriorityQueuecode.java
@@ -1,14 +1,33 @@
+// import java.util.*;
+// public class PriorityQueuecode{
+//     public static void main(String[] args){
+//         PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+//         pq.offer(5);
+//         pq.offer(3);
+//         pq.offer(1);
+//         pq.offer(6);
+//         System.out.println(pq);
+//         // System.out.println(pq.poll());
+//         // System.out.println(pq.peek());
+//         System.out.println(pq.isEmpty());
+//         while(!pq.isEmpty()){
+//             System.out.print(pq.poll()+",");
+//         }
+//     }
+// }
+// // To convert it into maxheap
 import java.util.*;
 public class PriorityQueuecode{
     public static void main(String[] args){
-        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>(Comparator.reverseOrder());
         pq.offer(5);
         pq.offer(3);
         pq.offer(1);
         pq.offer(6);
         System.out.println(pq);
-        System.out.println(pq.poll());
-        System.out.println(pq.peek());
         System.out.println(pq.isEmpty());
+        while(!pq.isEmpty()){
+            System.out.print(pq.poll()+",");
+        }
     }
 }
