799a022aacb13a24b324e65d197389856c919ab7
Aryan <aryansinghal207@gmail.com>
2025-05-07 15:57:02 +0530

    java

--- Queuecode.java
+++ Queuecode.java
@@ -473,9 +473,38 @@
 // Queue Using two pointer
 import java.util.*;
 public class Queuecode{
+    static class Queue{
+        static Stack<Integer> s1=new Stack<>();
+        static Stack<Integer> s2=new Stack<>();
+        public static boolean isEmpty(){
+            return s1.isEmpty();
+        }
+        public static void add(int data){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Queue Empty");
+                return -1;
+            }
+            return s1.pop();
+        }
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Queue Empty");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
     public static void main(String[] args){
-        // Queue<Integer> q=new LinkedList<>();
-        Queue<Integer> q=new ArrayDeque<>(); 
+        Queue q=new Queue(); 
         q.add(1);
         q.add(2);
         q.add(3);
