44966284c999e20cd6dd959757f974f47fef9dc7
Aryan <aryansinghal207@gmail.com>
2025-05-24 17:38:35 +0530

    g

--- Queuecode.java
+++ Queuecode.java
@@ -636,29 +636,64 @@
 // }
 
 
-// First Non repeating number
+// // First Non repeating number
+// import java.util.*;
+// public class Queuecode{
+//     public static void printNonrepeating(String str){
+//         int freq[]=new int[26];
+//         Queue<Character> q=new LinkedList<>();
+//         for(int i=0;i<str.length();i++){
+//             char ch=str.charAt(i);
+//             q.add(ch);
+//             freq[ch-'a']++;
+//             while(!q.isEmpty() && freq[q.peek()-'a']>1){
+//                 q.remove();
+//             }
+//             if(q.isEmpty()){
+//                 System.out.print(-1+" ");
+//             }else{
+//                 System.out.print(q.peek()+" ");
+//             }
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String []args){
+//         String str="aabccxb";
+//         printNonrepeating(str);
+//     }
+// }
+
+
+// Interleave 2 halves of a queue
 import java.util.*;
 public class Queuecode{
-    public static void printNonrepeating(String str){
-        int freq[]=new int[26];
-        Queue<Character> q=new LinkedList<>();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            q.add(ch);
-            freq[ch-'a']++;
-            while(!q.isEmpty() && freq[q.peek()-'a']>1){
-                q.remove();
-            }
-            if(q.isEmpty()){
-                System.out.print(-1+" ");
-            }else{
-                System.out.print(q.peek()+" ");
-            }
+    public static void Interleave(Queue<Integer> q){
+        Queue<Integer> firsthalf=new LinkedList<>();
+        int size=q.size();
+        for(int i=0;i<size/2;i++){
+            firsthalf.add(q.remove());
         }
-        System.out.println();
+        while(!firsthalf.isEmpty()){
+            q.add(firsthalf.remove());
+            q.add(q.remove());
+        }
     }
-    public static void main(String []args){
-        String str="aabccxb";
-        printNonrepeating(str);
+    public static void main(String[] args){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        Interleave(q);
+        while(!q.isEmpty()){
+            System.out.print(q.remove()+" ");
+        }
+        System.out.print("");
     }
 }
