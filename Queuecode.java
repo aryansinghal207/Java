5d391f0528bfb15fc857f0710a4fdf82e5e19b26
Aryan <aryansinghal207@gmail.com>
2025-05-24 17:54:18 +0530

    g

--- Queuecode.java
+++ Queuecode.java
@@ -700,3 +700,34 @@
 
 
 // Queue Reversal
+import java.util.*;
+public class Queuecode{
+    public static void reverse(Queue<Integer> q){
+        Stack<Integer> s=new Stack<>();
+        int size=q.size();
+        while(!q.isEmpty()){
+            s.add(q.remove());
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+    }
+    public static void main(String[] args){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        reverse(q);
+        while(!q.isEmpty()){
+            System.out.print(q.remove()+" ");
+        }
+        System.out.print("");
+    }
+}
