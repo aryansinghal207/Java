6b7889877bc5ff76efe3e11cd7bd9413bac27ce4
Aryan <aryansinghal207@gmail.com>
2025-05-25 13:10:23 +0530

    g

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -30,11 +30,31 @@ public class BinaryTreecode{
             preorder(root.left);
             preorder(root.right);
         }
+        public static void inorder(Node root){
+            if(root == null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+        public static void postorder(Node root){
+            if(root == null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
     }
     public static void main(String[] args){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root=tree.buildtree(nodes);
-        tree.preorder(root); 
+        tree.preorder(root);
+        System.out.println(""); 
+        tree.inorder(root); 
+        System.out.println(""); 
+        tree.postorder(root);
     }
 }
