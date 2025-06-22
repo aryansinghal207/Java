36e7ba78343249e02644d1cc7f2dcaa2ba7beae9
Aryan <aryansinghal207@gmail.com>
2025-06-22 21:48:37 +0530

    Bit

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class BitManipulation{
+    public static void oddoreven(int n){
+        if((n & 1)==0){
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
+    }
     public static void main(String[] args){
-        
+        oddoreven(1);
+        oddoreven(2);
+        oddoreven(3);
+        oddoreven(4);
     }
 }
