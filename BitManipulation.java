2d56f1a17964ed1b392f0e54e9ef0f3436538227
Aryan <aryansinghal207@gmail.com>
2025-06-26 21:42:07 +0530

    java

--- BitManipulation.java
+++ BitManipulation.java
@@ -26,3 +26,15 @@
 //         System.out.println(setithbit(10,2));
 //     }
 // }
+
+
+import java.util.*;
+public class BitManipulation{
+    public static int clearithbit(int n,int i){
+        int bitmask=~(1<<i);
+        return n & bitmask;
+    }
+    public static void main(String[] args){
+        System.out.println(clearithbit(10,1));
+    }
+}
