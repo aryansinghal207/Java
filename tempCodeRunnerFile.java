f73ce0a6299f5fe8a11d763485a4d2365c70acba
Aryan <aryansinghal207@gmail.com>
2025-06-27 21:33:17 +0530

    java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,30 +1,49 @@
-// Shortest Path
+// N-Queen
 import java.util.*;
-public class Stringfile{ 
-    public static float getshortestpath(String path){
-        int x=0;
-        int y=0;
-        for(int i=0;i<path.length();i++){
-            char dir=path.charAt(i);
-            if(dir=='N'){
-                y++;
-            }
-            else if(dir=='S'){
-                y--;
-            }
-            else if(dir=='E'){
-                x++;
-            }
-            else{
-                x--;
+public class Backtracking{
+    public static boolean issafe(char board[][], int row, int col){
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+    public static void nqueen(char board[][],int row){
+        if(row==board.length){
+            printboard(board);
+            return;
+        }
+        for(int j=0;j<board.length;j++){
+            if(issafe(board,row,j)){
+                board[row][j]='Q';
+                nqueen(board,row+1);
+                board[row][j]='.';
             }
         }
-        int x2=x*x;
-        int y2=y*y;
-        return (float)Math.sqrt(x2+y2);        
     }
-    public static void main(String[] args) {
-        String path="WNEENESENNN";
-        System.out.println(getshortestpath(path));
+    public static void printboard(char board[][]){
+        System.out.println("----- Chess Board -----");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int n=2;
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        nqueen(board,0);
     }
 }
