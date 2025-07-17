045cd1b290ed90ca33574394bc401d1850847679
Aryan <aryansinghal207@gmail.com>
2025-07-17 11:57:26 +0530

    java

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -166,35 +166,37 @@
 // }
 
 
-// Count the nodes of a tree
-public class BinaryTreecode {
+// // Count the nodes of a tree
+// public class BinaryTreecode {
 
-   static class Node {
-       int data;
-       Node left;
-       Node right;
+//    static class Node {
+//        int data;
+//        Node left;
+//        Node right;
 
-       Node(int data) {
-           this.data = data;
-           this.left = null;
-       }
-   }
+//        Node(int data) {
+//            this.data = data;
+//            this.left = null;
+//        }
+//    }
 
-   public static int count(Node root){
-       if(root == null) return 0;
-       int leftcount=count(root.left);
-       int rightcount=count(root.right);
-       return leftcount+rightcount+1;
-   }
+//    public static int count(Node root){
+//        if(root == null) return 0;
+//        int leftcount=count(root.left);
+//        int rightcount=count(root.right);
+//        return leftcount+rightcount+1;
+//    }
 
-   public static void main(String[] args) {
-       Node root=new Node(1);
-       root.left=new Node(2);
-       root.right=new Node(3);
-       root.left.left=new Node(4);
-       root.left.right=new Node(5);
-       root.right.left=new Node(6);
-       root.right.right=new Node(7);
-       System.out.println(count(root));
-   }
-}
+//    public static void main(String[] args) {
+//        Node root=new Node(1);
+//        root.left=new Node(2);
+//        root.right=new Node(3);
+//        root.left.left=new Node(4);
+//        root.left.right=new Node(5);
+//        root.right.left=new Node(6);
+//        root.right.right=new Node(7);
+//        System.out.println(count(root));
+//    }
+// }
+
+
