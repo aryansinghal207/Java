55128880314856a790703c20b8cb7a8a8c10d2ea
Aryan <aryansinghal207@gmail.com>
2025-07-17 11:55:58 +0530

    java

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -131,34 +131,36 @@
 //     }
 // }
 
-public class BinaryTreecode {
-    static class Node {
-        int data;
-        Node left;
-        Node right;
 
-        Node(int data) {
-            this.data = data;
-            this.left = null;
-        }
-    }
-    public static int height(Node root){
-        if(root==null){
-            return 0;
-        }
-        int lh=height(root.left);
-        int rh=height(root.right);
-        return Math.max(lh,rh)+1;
-    }
+// // Height of a tree
+// public class BinaryTreecode {
+//     static class Node {
+//         int data;
+//         Node left;
+//         Node right;
 
-    public static void main(String[] args) {
-        Node root=new Node(1);
-        root.left=new Node(2);
-        root.right=new Node(3);
-        root.left.left=new Node(4);
-        root.left.right=new Node(5);
-        root.right.left=new Node(6);
-        root.right.right=new Node(7);
-        System.out.println(height(root));
-    }
-}
+//         Node(int data) {
+//             this.data = data;
+//             this.left = null;
+//         }
+//     }
+//     public static int height(Node root){
+//         if(root==null){
+//             return 0;
+//         }
+//         int lh=height(root.left);
+//         int rh=height(root.right);
+//         return Math.max(lh,rh)+1;
+//     }
+
+//     public static void main(String[] args) {
+//         Node root=new Node(1);
+//         root.left=new Node(2);
+//         root.right=new Node(3);
+//         root.left.left=new Node(4);
+//         root.left.right=new Node(5);
+//         root.right.left=new Node(6);
+//         root.right.right=new Node(7);
+//         System.out.println(height(root));
+//     }
+// }
