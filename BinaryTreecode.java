f122a337b072a2ec9315b4970acd49a762e6be1b
Aryan <aryansinghal207@gmail.com>
2025-07-18 08:09:17 +0530

    java

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -235,45 +235,47 @@
 // }
 
 
-// Diameter of a tree TC : O(n^2)
-public class BinaryTreecode{
-    static class Node{
-        int data;
-        Node left;
-        Node right;
-        Node(int data){
-            this.data=data;
-            this.left=null;
-        }
-    }
-    public static int height(Node root){
-        if(root == null){
-            return 0;
-        }
-        int lh=height(root.left);
-        int rh=height(root.right);
-        return Math.max(lh,rh)+1;
-    }
-    public static int diameter(Node root){
-        if(root == null){
-            return 0;
-        }
-        int leftdia=diameter(root.left);
-        int lh=height(root.left);
-        int rightdia=diameter(root.right);
-        int rh=height(root.right);
-        int selfdia=lh+rh+1;
-        return Math.max(selfdia,Math.max(leftdia,rightdia));
-    }
+// // Diameter of a tree TC : O(n^2)
+// public class BinaryTreecode{
+//     static class Node{
+//         int data;
+//         Node left;
+//         Node right;
+//         Node(int data){
+//             this.data=data;
+//             this.left=null;
+//         }
+//     }
+//     public static int height(Node root){
+//         if(root == null){
+//             return 0;
+//         }
+//         int lh=height(root.left);
+//         int rh=height(root.right);
+//         return Math.max(lh,rh)+1;
+//     }
+//     public static int diameter(Node root){
+//         if(root == null){
+//             return 0;
+//         }
+//         int leftdia=diameter(root.left);
+//         int lh=height(root.left);
+//         int rightdia=diameter(root.right);
+//         int rh=height(root.right);
+//         int selfdia=lh+rh+1;
+//         return Math.max(selfdia,Math.max(leftdia,rightdia));
+//     }
 
-    public static void main(String[] args) {
-        Node root=new Node(1);
-        root.left=new Node(2);
-        root.right=new Node(3);
-        root.left.left=new Node(4);
-        root.left.right=new Node(5);
-        root.right.left=new Node(6);
-        root.right.right=new Node(7);
-        System.out.println(diameter(root));
-    }
-}
+//     public static void main(String[] args) {
+//         Node root=new Node(1);
+//         root.left=new Node(2);
+//         root.right=new Node(3);
+//         root.left.left=new Node(4);
+//         root.left.right=new Node(5);
+//         root.right.left=new Node(6);
+//         root.right.right=new Node(7);
+//         System.out.println(diameter(root));
+//     }
+// }
+
+
