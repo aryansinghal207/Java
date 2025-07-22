013cd1a8aa3dca7a26c560e0a6d7f2c83275482e
Aryan <aryansinghal207@gmail.com>
2025-07-22 14:36:52 +0530

    Java

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -419,7 +419,43 @@
 // }
 
 
-// Kth level
+// // Kth level
+// import java.util.*;
+// public class BinaryTreecode {
+//     public static class Node {
+//         int data;
+//         Node left, right;
+//         Node(int data) {
+//             this.data = data;
+//             this.left = this.right = null;
+//         }
+//     }
+//     public static void klevel(Node root,int level,int k){
+//         if(root==null){
+//             return;
+//         }
+//         if(level==k){
+//             System.out.print(root.data+" ");
+//             return;
+//         }
+//         klevel(root.left,level+1,k);
+//         klevel(root.right,level+1,k);
+//     }
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         root.left = new Node(2);
+//         root.right = new Node(3);
+//         root.left.left = new Node(4);
+//         root.left.right = new Node(5);
+//         root.right.left = new Node(6);
+//         root.right.right = new Node(7);
+//         int k=3;
+//         klevel(root,1,k);
+//     }
+// }
+
+
+// lowest Coomon Ancestor
 import java.util.*;
 public class BinaryTreecode {
     public static class Node {
@@ -430,16 +466,35 @@ public class BinaryTreecode {
             this.left = this.right = null;
         }
     }
-    public static void klevel(Node root,int level,int k){
+    public static boolean getpath(Node root,int n,ArrayList<Node> path){
         if(root==null){
-            return;
+            return false;
         }
-        if(level==k){
-            System.out.print(root.data+" ");
-            return;
+        path.add(root);
+        if(root.data==n){
+            return true;
         }
-        klevel(root.left,level+1,k);
-        klevel(root.right,level+1,k);
+        boolean foundleft=getpath(root.left,n,path);
+        boolean foundright=getpath(root.right,n,path);
+        if(foundleft || foundright){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+    public static Node lca(Node root,int n1,int n2){
+        ArrayList<Node> path1=new ArrayList<>();
+        ArrayList<Node> path2=new ArrayList<>();
+        getpath(root,n1,path1);
+        getpath(root,n2,path2);
+        int i=0;
+        for(i=0;i<path1.size() && i<path2.size();i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        Node lca=path1.get(i-1);
+        return lca;
     }
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -449,7 +504,7 @@ public class BinaryTreecode {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        int k=3;
-        klevel(root,1,k);
+        int n1=4,n2=7;
+        System.out.print(lca(root,n1,n2).data);
     }
 }
