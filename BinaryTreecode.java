851a3118659c7ee8e242c60c9d316f2f609a582c
Aryan <aryansinghal207@gmail.com>
2025-07-22 15:12:40 +0530

    Java

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -455,7 +455,62 @@
 // }
 
 
-// lowest Coomon Ancestor
+// // lowest Coomon Ancestor with Time complexity and space complexity = O(n)
+// import java.util.*;
+// public class BinaryTreecode {
+//     public static class Node {
+//         int data;
+//         Node left, right;
+//         Node(int data) {
+//             this.data = data;
+//             this.left = this.right = null;
+//         }
+//     }
+//     public static boolean getpath(Node root,int n,ArrayList<Node> path){
+//         if(root==null){
+//             return false;
+//         }
+//         path.add(root);
+//         if(root.data==n){
+//             return true;
+//         }
+//         boolean foundleft=getpath(root.left,n,path);
+//         boolean foundright=getpath(root.right,n,path);
+//         if(foundleft || foundright){
+//             return true;
+//         }
+//         path.remove(path.size()-1);
+//         return false;
+//     }
+//     public static Node lca(Node root,int n1,int n2){
+//         ArrayList<Node> path1=new ArrayList<>();
+//         ArrayList<Node> path2=new ArrayList<>();
+//         getpath(root,n1,path1);
+//         getpath(root,n2,path2);
+//         int i=0;
+//         for(;i<path1.size() && i<path2.size();i++){
+//             if(path1.get(i) != path2.get(i)){
+//                 break;
+//             }
+//         }
+//         Node lca=path1.get(i-1);
+//         return lca;
+//     }
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         root.left = new Node(2);
+//         root.right = new Node(3);
+//         root.left.left = new Node(4);
+//         root.left.right = new Node(5);
+//         root.right.left = new Node(6);
+//         root.right.right = new Node(7);
+//         int n1=4,n2=7;
+//         System.out.print(lca(root,n1,n2).data);
+//     }
+// }
+
+
+// lowest Common Ancestor with Time complexity O(n) and space complexity O(1)
 import java.util.*;
 public class BinaryTreecode {
     public static class Node {
@@ -482,19 +537,13 @@ public class BinaryTreecode {
         path.remove(path.size()-1);
         return false;
     }
-    public static Node lca(Node root,int n1,int n2){
-        ArrayList<Node> path1=new ArrayList<>();
-        ArrayList<Node> path2=new ArrayList<>();
-        getpath(root,n1,path1);
-        getpath(root,n2,path2);
-        int i=0;
-        for(;i<path1.size() && i<path2.size();i++){
-            if(path1.get(i) != path2.get(i)){
-                break;
-            }
-        }
-        Node lca=path1.get(i-1);
-        return lca;
+    public static Node lca2(Node root,int n1,int n2){
+        if(root==null || root.data==n1 || root.data==n2) return root;
+        Node leftlca=lca2(root.left,n1,n2);
+        Node rightlca=lca2(root.right,n1,n2);
+        if(rightlca==null) return leftlca;
+        if(leftlca==null) return rightlca;
+        return root;
     }
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -505,6 +554,6 @@ public class BinaryTreecode {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
         int n1=4,n2=7;
-        System.out.print(lca(root,n1,n2).data);
+        System.out.print(lca2(root,n1,n2).data);
     }
 }
