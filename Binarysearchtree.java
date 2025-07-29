18ee57ccc9e38602c5ebca11de509936795caa22
Aryan <aryansinghal207@gmail.com>
2025-07-29 11:13:09 +0530

    Java

--- Binarysearchtree.java
+++ Binarysearchtree.java
@@ -157,3 +157,78 @@
 
 
 // Delete a Node
+public class Binarysearchtree{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static Node insert(Node root,int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left=insert(root.left,val);
+        }else if(root.data<val){
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static Node Delete(Node root,int val){
+        if (root == null) {
+            return null;
+        }
+        if(root.data<val){
+            root.right=Delete(root.right,val);
+        }
+        else if(root.data>val){
+            root.left=Delete(root.left,val);
+        }
+        else{
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            Node IS=findInordersuccessor(root.right);
+            root.data=IS.data;
+            root.right=Delete(root.right,IS.data);
+        }
+        return root;
+    }
+    public static Node findInordersuccessor(Node root){
+        while(root.left != null){
+            root=root.left;
+        }
+        return root;
+    }
+    public static void main(String[] args){
+        int values[]={8,5,3,1,4,6,10,11,14};
+        Node root=null;
+        for(int i=0;i<values.length;i++){
+            root=insert(root,values[i]);
+        }
+        System.out.println("Original Inorder:");
+        inorder(root);
+
+        System.out.println("\n\nAfter deleting 1:");
+        root = Delete(root, 10);
+        inorder(root);
+    }
+}
