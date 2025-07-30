7e4c91ab54c4ca15630fa988753aa436a1bef2e4
Aryan <aryansinghal207@gmail.com>
2025-07-30 10:17:34 +0530

    Java

--- Binarysearchtree.java
+++ Binarysearchtree.java
@@ -295,64 +295,66 @@
 // }
 
 
-// Root to leaf path
-import java.util.*;
-public class Binarysearchtree{
-    static class Node{
-        int data;
-        Node left;
-        Node right;
-        Node(int data){
-            this.data=data;
-        }
-    }
-    public static Node insert(Node root,int val){
-        if(root==null){
-            root=new Node(val);
-            return root;
-        }
-        if(root.data>val){
-            root.left=insert(root.left,val);
-        }else if(root.data<val){
-            root.right=insert(root.right,val);
-        }
-        return root;
-    }
-    public static void inorder(Node root){
-        if(root == null){
-            return;
-        }
-        inorder(root.left);
-        System.out.print(root.data+" ");
-        inorder(root.right);
-    }
-    public static void printpath(ArrayList<Integer> path){
-        for(int i=0;i<path.size();i++){
-            System.out.print(path.get(i)+"-");
-        }
-        System.out.println("Null");
-    }
-    public static void printroot2leaf(Node root,ArrayList<Integer> path){
-        if(root==null){
-            return;
-        }
-        path.add(root.data);
-        if(root.left == null && root.right == null){
-            printpath(path);
-        }
-        printroot2leaf(root.left,path);
-        printroot2leaf(root.right,path);
-        path.remove(path.size()-1);
-    }
-    public static void main(String[] args){
-        int values[]={8,5,3,1,4,6,10,11,14};
-        Node root=null;
-        for(int i=0;i<values.length;i++){
-            root=insert(root,values[i]);
-        }
-        System.out.println("Original Inorder:");
-        inorder(root);
-        System.out.println("");
-        printroot2leaf(root,new ArrayList<>());
-    }
-}
+// // Root to leaf path
+// import java.util.*;
+// public class Binarysearchtree{
+//     static class Node{
+//         int data;
+//         Node left;
+//         Node right;
+//         Node(int data){
+//             this.data=data;
+//         }
+//     }
+//     public static Node insert(Node root,int val){
+//         if(root==null){
+//             root=new Node(val);
+//             return root;
+//         }
+//         if(root.data>val){
+//             root.left=insert(root.left,val);
+//         }else if(root.data<val){
+//             root.right=insert(root.right,val);
+//         }
+//         return root;
+//     }
+//     public static void inorder(Node root){
+//         if(root == null){
+//             return;
+//         }
+//         inorder(root.left);
+//         System.out.print(root.data+" ");
+//         inorder(root.right);
+//     }
+//     public static void printpath(ArrayList<Integer> path){
+//         for(int i=0;i<path.size();i++){
+//             System.out.print(path.get(i)+"-");
+//         }
+//         System.out.println("Null");
+//     }
+//     public static void printroot2leaf(Node root,ArrayList<Integer> path){
+//         if(root==null){
+//             return;
+//         }
+//         path.add(root.data);
+//         if(root.left == null && root.right == null){
+//             printpath(path);
+//         }
+//         printroot2leaf(root.left,path);
+//         printroot2leaf(root.right,path);
+//         path.remove(path.size()-1);
+//     }
+//     public static void main(String[] args){
+//         int values[]={8,5,3,1,4,6,10,11,14};
+//         Node root=null;
+//         for(int i=0;i<values.length;i++){
+//             root=insert(root,values[i]);
+//         }
+//         System.out.println("Original Inorder:");
+//         inorder(root);
+//         System.out.println("");
+//         printroot2leaf(root,new ArrayList<>());
+//     }
+// }
+
+
