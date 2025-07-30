01fcde1e3ec41dbf90f0c843e357c1979d665983
Aryan <aryansinghal207@gmail.com>
2025-07-30 10:27:46 +0530

    Java

--- Binarysearchtree.java
+++ Binarysearchtree.java
@@ -358,3 +358,62 @@
 // }
 
 
+// Root to leaf path
+import java.util.*;
+public class Binarysearchtree{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static Node insert(Node root,int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left=insert(root.left,val);
+        }else if(root.data<val){
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static boolean isvalidBST(Node root, Node min, Node max){
+        if(root==null){
+            return true;
+        }
+        if(min != null && root.data<=min.data){
+            return false;
+        }
+        else if(max != null && root.data>=max.data){
+            return false;
+        }
+        return isvalidBST(root.left,min,root) && isvalidBST(root.right,root,max);
+    }
+    public static void main(String[] args){
+        int values[]={8,5,3,1,4,6,10,11,14};
+        Node root=null;
+        for(int i=0;i<values.length;i++){
+            root=insert(root,values[i]);
+        }
+        System.out.println("Original Inorder:");
+        inorder(root);
+        System.out.println("");
+        if(isvalidBST(root,null,null)){
+            System.out.println("Valid");
+        }else{
+            System.out.println("Not Valid");
+        }
+    }
+}
