51ab1d3f58a4c5003d240ca73db4add413d0fbc2
Aryan <aryansinghal207@gmail.com>
2025-08-03 16:09:33 +0530

    Queue

--- PriorityQueuecode.java
+++ PriorityQueuecode.java
@@ -33,6 +33,34 @@
 //     }
 // }
 
+// // PQ for Object
+// import java.util.*;
+// public class PriorityQueuecode{
+//     static class Student implements Comparable<Student>{
+//         String name;
+//         int rank;
+//         public Student(String name,int rank){
+//             this.name=name;
+//             this.rank=rank;
+//         }
+//         @Override
+//         public int compareTo(Student s2){
+//             return this.rank-s2.rank;
+//         }
+//     }
+//     public static void main(String args[]){
+//         PriorityQueue<Student> pq=new PriorityQueue<>();
+//         pq.add(new Student("A",4));
+//         pq.add(new Student("B",5));
+//         pq.add(new Student("C",2));
+//         pq.add(new Student("D",12));
+//         while(!pq.isEmpty()){
+//             System.out.println(pq.peek().name+"-> "+pq.peek().rank);
+//             pq.remove();
+//         }
+//     }
+// }
+
 // PQ for Object
 import java.util.*;
 public class PriorityQueuecode{
@@ -49,7 +77,7 @@ public class PriorityQueuecode{
         }
     }
     public static void main(String args[]){
-        PriorityQueue<Student> pq=new PriorityQueue<>();
+        PriorityQueue<Student> pq=new PriorityQueue<>(Comparator.reverseOrder());
         pq.add(new Student("A",4));
         pq.add(new Student("B",5));
         pq.add(new Student("C",2));
