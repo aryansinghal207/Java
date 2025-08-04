3529cde790a6b5f273d8276baacf126e7195151d
Aryan <aryansinghal207@gmail.com>
2025-08-04 13:37:55 +0530

    Queue

--- Hashingcode.java
+++ Hashingcode.java
@@ -25,32 +25,50 @@
 // }
 
 
-// All Operations in HashMap
+// // All Operations in HashMap
+// import java.util.*;
+// public class Hashingcode{
+//     public static void main(String args[]){
+//         // Create
+//         HashMap<String,Integer> hm=new HashMap<>();
+//         // Insert - O(1)
+//         hm.put("India",100);
+//         hm.put("China",90);
+//         hm.put("US",60);
+//         hm.put("Nepal",20);
+//         System.out.println(hm);
+//         // Get - O(1)
+//         int pop=hm.get("India");
+//         System.out.println(pop);
+//         //containsKey - O(1)
+//         System.out.println(hm.containsKey("India"));
+//         System.out.println(hm.containsKey("Indonesia"));
+//         // remove - O(1)
+//         System.out.println(hm.remove("India"));
+//         System.out.println(hm);
+//         // size
+//         System.out.println(hm.size());
+//         // Is Empty
+//         System.out.println(hm.isEmpty());
+//         hm.clear();
+//         System.out.println(hm.isEmpty());
+//     }
+// }
+
+
+// Iteration on HashMaps
 import java.util.*;
 public class Hashingcode{
     public static void main(String args[]){
-        // Create
         HashMap<String,Integer> hm=new HashMap<>();
-        // Insert - O(1)
         hm.put("India",100);
         hm.put("China",90);
         hm.put("US",60);
         hm.put("Nepal",20);
-        System.out.println(hm);
-        // Get - O(1)
-        int pop=hm.get("India");
-        System.out.println(pop);
-        //containsKey - O(1)
-        System.out.println(hm.containsKey("India"));
-        System.out.println(hm.containsKey("Indonesia"));
-        // remove - O(1)
-        System.out.println(hm.remove("India"));
-        System.out.println(hm);
-        // size
-        System.out.println(hm.size());
-        // Is Empty
-        System.out.println(hm.isEmpty());
-        hm.clear();
-        System.out.println(hm.isEmpty());
+        Set<String> keys=hm.keySet();
+        System.out.println(keys);
+        for(String k : keys){
+            System.out.println("Key = "+k+" , Values = "+hm.get(k));
+        }
     }
 }
