da6fd7b1a3ca80c9326f4f7bec21ed45d51fba9d
Aryan <aryansinghal207@gmail.com>
2025-08-04 11:28:17 +0530

    Queue

--- Hashingcode.java
+++ Hashingcode.java
@@ -11,15 +11,37 @@
 // }
 
 
-// HashMap and HashSet are unordered
+// // HashMap and HashSet are unordered
+// import java.util.*;
+// public class Hashingcode{
+//     public static void main(String args[]){
+//         HashMap<String,Integer> hm=new HashMap<>();
+//         hm.put("India",100);
+//         hm.put("China",90);
+//         hm.put("US",60);
+//         hm.put("Nepal",20);
+//         System.out.println(hm);
+//     }
+// }
+
+
+// All Operations in HashMap
 import java.util.*;
 public class Hashingcode{
     public static void main(String args[]){
+        // Create
         HashMap<String,Integer> hm=new HashMap<>();
+        // Insert - O(1)
         hm.put("India",100);
         hm.put("China",90);
         hm.put("US",60);
         hm.put("Nepal",20);
         System.out.println(hm);
+        // Get - O(1)
+        int pop=hm.get("India");
+        System.out.println(pop);
+        //containsKey - O(1)
+        System.out.println(hm.containsKey("India"));
+        System.out.println(hm.containsKey("Indonesia"));
     }
 }
