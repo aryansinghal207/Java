a0ce34da848f3fd2f64c6a609b0f12a06f27084e
Aryan <aryansinghal207@gmail.com>
2025-08-04 11:03:06 +0530

    Queue

--- Hashingcode.java
+++ Hashingcode.java
@@ -1,11 +1,25 @@
+// import java.util.*;
+// public class Hashingcode{
+//     public static void main(String args[]){
+//         HashMap<String,Integer> hm=new HashMap<>();
+//         hm.put("India",100);
+//         hm.put("India",101);
+//         hm.put("India",102);
+//         hm.put("India",103);   // HashMap does allow duplicate value and pick the last declared value of one
+//         System.out.println(hm);
+//     }
+// }
+
+
+// HashMap and HashSet are unordered
 import java.util.*;
 public class Hashingcode{
     public static void main(String args[]){
         HashMap<String,Integer> hm=new HashMap<>();
         hm.put("India",100);
-        hm.put("India",101);
-        hm.put("India",102);
-        hm.put("India",103);   // HashMap does allow duplicate value and pick the last declared value of one
+        hm.put("China",90);
+        hm.put("US",60);
+        hm.put("Nepal",20);
         System.out.println(hm);
     }
 }
