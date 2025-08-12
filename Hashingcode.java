48257590a3085b3b66ecade26cd8e0d3d1d17caf
Aryan <aryansinghal207@gmail.com>
2025-08-12 15:18:09 +0530

    initial

--- Hashingcode.java
+++ Hashingcode.java
@@ -161,5 +161,11 @@ public class Hashingcode{
         set.add(1);
         set.add(2);
         System.out.println(set);
+        if(set.contains(2)){
+            System.out.println("set contains 2");
+        }
+        if(set.contains(3)){
+            System.out.println("set contains 3");
+        }
     }
 }
