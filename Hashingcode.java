6c4d387f8934e9d119df1c581926f42c6dfc5b15
Aryan <aryansinghal207@gmail.com>
2025-08-12 23:44:23 +0530

    initial

--- Hashingcode.java
+++ Hashingcode.java
@@ -171,3 +171,16 @@
 // }
 
 
+// LinkedHashSet
+import java.util.*;
+public class Hashingcode{
+    public static void main(String[] args){
+        LinkedHashSet<String> set=new LinkedHashSet<>();
+        set.add("Delhi");
+        set.add("Pune");
+        set.add("Mumbai");
+        set.add("Odisha");
+        set.add("Banglore");
+        System.out.println(set);
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,13 @@
-8,5,3,1,4,6,10,11,14
+// LinkedHashSet
+import java.util.*;
+public class Hashingcode{
+    public static void main(String[] args){
+        LinkedHashSet<String> set=new LinkedHashSet<>();
+        set.add("Delhi");
+        set.add("Pune");
+        set.add("Mumbai");
+        set.add("Odisha");
+        set.add("Banglore");
+        System.out.println(set);
+    }
+}
