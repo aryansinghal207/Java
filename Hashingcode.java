9a2e5e2755d4dd591ae275a7477e0348cf5c80b4
Aryan <aryansinghal207@gmail.com>
2025-08-12 23:59:43 +0530

    initial

--- Hashingcode.java
+++ Hashingcode.java
@@ -201,15 +201,17 @@
 // }
 
 
-// Count Distinct Element
-import java.util.*;
-public class Hashingcode{
-    public static void main(String[] args){
-        int num[]={4,3,4,6,6,2,4,6,1,2,5,7,8};
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<num.length;i++){
-            set.add(num[i]);
-        }
-        System.out.println("ans = "+set.size());
-    }
-}
+// // Count Distinct Element
+// import java.util.*;
+// public class Hashingcode{
+//     public static void main(String[] args){
+//         int num[]={4,3,4,6,6,2,4,6,1,2,5,7,8};
+//         HashSet<Integer> set=new HashSet<>();
+//         for(int i=0;i<num.length;i++){
+//             set.add(num[i]);
+//         }
+//         System.out.println("ans = "+set.size());
+//     }
+// }
+
+
