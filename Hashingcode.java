7c688c1d31e7af06911c09df46ed42e77484acfe
Aryan <aryansinghal207@gmail.com>
2025-08-12 15:18:26 +0530

    initial

--- Hashingcode.java
+++ Hashingcode.java
@@ -150,22 +150,24 @@
 // }
 
 
-// HashSet : no duplicate values, unordered, unique elements, Null is allowed
-import java.util.*;
-public class Hashingcode{
-    public static void main(String[] args){
-        HashSet<Integer> set=new HashSet<>();
-        set.add(1);
-        set.add(2);
-        set.add(4);
-        set.add(1);
-        set.add(2);
-        System.out.println(set);
-        if(set.contains(2)){
-            System.out.println("set contains 2");
-        }
-        if(set.contains(3)){
-            System.out.println("set contains 3");
-        }
-    }
-}
+// // HashSet : no duplicate values, unordered, unique elements, Null is allowed
+// import java.util.*;
+// public class Hashingcode{
+//     public static void main(String[] args){
+//         HashSet<Integer> set=new HashSet<>();
+//         set.add(1);
+//         set.add(2);
+//         set.add(4);
+//         set.add(1);
+//         set.add(2);
+//         System.out.println(set);
+//         if(set.contains(2)){
+//             System.out.println("set contains 2");
+//         }
+//         if(set.contains(3)){
+//             System.out.println("set contains 3");
+//         }
+//     }
+// }
+
+
