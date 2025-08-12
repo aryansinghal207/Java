91ce5af13be638af80257766b49c28a6ea0ad0af
Aryan <aryansinghal207@gmail.com>
2025-08-12 15:06:28 +0530

    initial

--- Hashingcode.java
+++ Hashingcode.java
@@ -120,31 +120,46 @@
 // }
 
 
-// Valid Anagram using HashMap
+// // Valid Anagram using HashMap
+// import java.util.*;
+// public class Hashingcode{
+//     public static boolean isAnagram(String s,String t){
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+//         }
+//         for(int i=0;i<t.length();i++){
+//             if(map.containsKey(t.charAt(i))){
+//                 if(map.get(t.charAt(i))==1){
+//                     map.remove(t.charAt(i));
+//                 }else{
+//                     map.put(t.charAt(i),map.get(t.charAt(i))-1);
+//                 }
+//             }
+//         }
+//         return map.isEmpty();
+//     }
+//     public static void main(String[] args){
+//         String s="race";
+//         String t="care";
+//         System.out.println(isAnagram(s,t));
+//     }
+// }
+
+
+// HashSet : no duplicate values, unordered, unique elements, Null is allowed
 import java.util.*;
 public class Hashingcode{
-    public static boolean isAnagram(String s,String t){
-        if(s.length() != t.length()){
-            return false;
-        }
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-        }
-        for(int i=0;i<t.length();i++){
-            if(map.containsKey(t.charAt(i))){
-                if(map.get(t.charAt(i))==1){
-                    map.remove(t.charAt(i));
-                }else{
-                    map.put(t.charAt(i),map.get(t.charAt(i))-1);
-                }
-            }
-        }
-        return map.isEmpty();
-    }
     public static void main(String[] args){
-        String s="race";
-        String t="care";
-        System.out.println(isAnagram(s,t));
+        HashSet<Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(1);
+        set.add(2);
+        System.out.println(set);
     }
 }
