bae80b76415d6fb114e36697f1ba29971a754657
Aryan <aryansinghal207@gmail.com>
2025-08-19 12:46:25 +0530

    java

--- Hashingcode.java
+++ Hashingcode.java
@@ -284,6 +284,19 @@
 import java.util.*;
 public class Hashingcode{
     public static void main(String[] args){
-        
+        // int arr[]={15,-2,2,-8,1,7,10,23};
+        int arr[]={23,1,4,2,5,7};
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        int len=0;
+        for(int j=0;j<arr.length;j++){
+            sum+=arr[j];
+            if(map.containsKey(sum)){
+                len=Math.max(len,j-map.get(sum));
+            }else{
+                map.put(sum,j);
+            }
+        }
+        System.out.println("Largest subarray with 0 sum is : "+len);
     }
 }
