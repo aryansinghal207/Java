146898bc4f5408994ff4990b50c86eb61aad07a9
Aryan <aryansinghal207@gmail.com>
2025-08-24 17:07:24 +0530

    Java

--- Dynamicprogrammingcodes.java
+++ Dynamicprogrammingcodes.java
@@ -1,13 +1,33 @@
-// Climbing Stairs (Recursion)
+// // Climbing Stairs (Recursion)
+// import java.util.*;
+// public class Dynamicprogrammingcodes{
+//     public static int countways(int n){
+//         if(n==0) return 1;
+//         if(n<0) return 0;
+//         return countways(n-1)+countways(n-2);
+//     }
+//     public static void main(String[] args){
+//         int n=5;
+//         System.out.println(countways(n));
+//     }
+// }
+
+// Climbing Stairs (Memoization)
 import java.util.*;
 public class Dynamicprogrammingcodes{
-    public static int countways(int n){
+    public static int countways(int n,int arr[]){
         if(n==0) return 1;
         if(n<0) return 0;
-        return countways(n-1)+countways(n-2);
+        if(arr[n]!=-1){
+            return arr[n];
+        }
+        arr[n] = countways(n-1,arr)+countways(n-2,arr);
+        return arr[n];
     }
     public static void main(String[] args){
         int n=5;
-        System.out.println(countways(n));
+        int ways[]=new int[n+1];
+        Arrays.fill(ways,-1);
+        System.out.println(countways(n,ways));
     }
 }
