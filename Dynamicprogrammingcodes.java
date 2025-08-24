296fb4504d32cd7c06d644d49e7cdf74732111c5
Aryan <aryansinghal207@gmail.com>
2025-08-24 16:59:28 +0530

    Java

---
+++ Dynamicprogrammingcodes.java
@@ -0,0 +1,13 @@
+// Climbing Stairs (Recursion)
+import java.util.*;
+public class Dynamicprogrammingcodes{
+    public static int countways(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        return countways(n-1)+countways(n-2);
+    }
+    public static void main(String[] args){
+        int n=5;
+        System.out.println(countways(n));
+    }
+}
