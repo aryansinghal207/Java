99515aa9831697c253bb8f58776f82cdd75190be
Aryan <aryansinghal207@gmail.com>
2025-08-27 14:53:00 +0530

    Java

--- Dynamicprogrammingcodes.java
+++ Dynamicprogrammingcodes.java
@@ -129,29 +129,31 @@
 // }
 
 
-// 0-1 Knapsack (Tabulation)
-import java.util.*;
-public class Dynamicprogrammingcodes{
-    public static int knapsack(int[] val,int[] wt,int w){
-        int n=val.length;
-        int dp[][]=new int[n+1][w+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=w;j++){
-                if(wt[i-1]<=j){
-                    int ans1=val[i-1]+dp[i-1][j-wt[i-1]];
-                    int ans2=dp[i-1][j];
-                    dp[i][j]=Math.max(ans1,ans2);
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][w];
-    }
-    public static void main(String[] args){
-        int val[]={15,14,10,45,30};
-        int wt[]={2,5,1,3,4};
-        int w=7;
-        System.out.println(knapsack(val,wt,w));
-    }
-}
+// // 0-1 Knapsack (Tabulation)
+// import java.util.*;
+// public class Dynamicprogrammingcodes{
+//     public static int knapsack(int[] val,int[] wt,int w){
+//         int n=val.length;
+//         int dp[][]=new int[n+1][w+1];
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=w;j++){
+//                 if(wt[i-1]<=j){
+//                     int ans1=val[i-1]+dp[i-1][j-wt[i-1]];
+//                     int ans2=dp[i-1][j];
+//                     dp[i][j]=Math.max(ans1,ans2);
+//                 }else{
+//                     dp[i][j]=dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][w];
+//     }
+//     public static void main(String[] args){
+//         int val[]={15,14,10,45,30};
+//         int wt[]={2,5,1,3,4};
+//         int w=7;
+//         System.out.println(knapsack(val,wt,w));
+//     }
+// }
+
+
