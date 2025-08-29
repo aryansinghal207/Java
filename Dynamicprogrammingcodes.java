4e17d135187d23eed0b654291372e35ff9a6085a
Aryan <aryansinghal207@gmail.com>
2025-08-29 12:10:57 +0530

    Java

--- Dynamicprogrammingcodes.java
+++ Dynamicprogrammingcodes.java
@@ -157,3 +157,30 @@
 // }
 
 
+// Target Sum Subset (Tabulation)
+import java.util.*;
+public class Dynamicprogrammingcodes{
+    public static boolean subsetSum(int arr[],int sum){
+        int n=arr.length;
+        boolean dp[][]=new boolean[n+1][sum+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                int v=arr[i-1];
+                if(v<=j){
+                    dp[i][j]=dp[i-1][j-v] || dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+    public static void main(String[] args){
+        int arr[]={2,3,5,6,8,10};
+        int sum=10;
+        System.out.println(subsetSum(arr,sum));
+    }
+}
