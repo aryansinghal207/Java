b8ab3e3b07708c880a125b692bc31a2108243479
Aryan <aryansinghal207@gmail.com>
2025-10-13 08:23:16 +0530

    Java

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -138,7 +138,6 @@
 //         int data;
 //         Node left;
 //         Node right;
-
 //         Node(int data) {
 //             this.data = data;
 //             this.left = null;
@@ -152,7 +151,6 @@
 //         int rh=height(root.right);
 //         return Math.max(lh,rh)+1;
 //     }
-
 //     public static void main(String[] args) {
 //         Node root=new Node(1);
 //         root.left=new Node(2);
@@ -511,33 +509,33 @@
 
 
 // // lowest Common Ancestor with Time complexity O(n) and space complexity O(1)
-import java.util.*;
-public class BinaryTreecode {
-    public static class Node {
-        int data;
-        Node left, right;
-        Node(int data) {
-            this.data = data;
-            this.left = this.right = null;
-        }
-    }
-    public static Node lca2(Node root,int n1,int n2){
-        if(root==null || root.data==n1 || root.data==n2) return root;
-        Node leftlca=lca2(root.left,n1,n2);
-        Node rightlca=lca2(root.right,n1,n2);
-        if(rightlca==null) return leftlca;
-        if(leftlca==null) return rightlca;
-        return root;
-    }
-    public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
-        root.right.left = new Node(6);
-        root.right.right = new Node(7);
-        int n1=4,n2=7;
-        System.out.print(lca2(root,n1,n2).data);
-    }
-}
+// import java.util.*;
+// public class BinaryTreecode {
+//     public static class Node {
+//         int data;
+//         Node left, right;
+//         Node(int data) {
+//             this.data = data;
+//             this.left = this.right = null;
+//         }
+//     }
+//     public static Node lca2(Node root,int n1,int n2){
+//         if(root==null || root.data==n1 || root.data==n2) return root;
+//         Node leftlca=lca2(root.left,n1,n2);
+//         Node rightlca=lca2(root.right,n1,n2);
+//         if(rightlca==null) return leftlca;
+//         if(leftlca==null) return rightlca;
+//         return root;
+//     }
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         root.left = new Node(2);
+//         root.right = new Node(3);
+//         root.left.left = new Node(4);
+//         root.left.right = new Node(5);
+//         root.right.left = new Node(6);
+//         root.right.right = new Node(7);
+//         int n1=4,n2=7;
+//         System.out.print(lca2(root,n1,n2).data);
+//     }
+// }
