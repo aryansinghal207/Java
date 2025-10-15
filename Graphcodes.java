f199924e97387ac5877de7c7c2d461a2bb1aa46c
Aryan <aryansinghal207@gmail.com>
2025-10-15 14:22:54 +0530

    Graph code

--- Graphcodes.java
+++ Graphcodes.java
@@ -35,7 +35,58 @@
 // }
 
 
-// BFS : Breadth First Search
+// // BFS : Breadth First Search
+// import java.util.*;
+// public class Graphcodes {
+//     static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s , int d , int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i=0;i<graph.length;i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1, 5));
+//         graph[1].add(new Edge(1, 0, 5));
+//         graph[1].add(new Edge(1, 2, 1));
+//         graph[1].add(new Edge(1, 3, 3));
+//         graph[2].add(new Edge(2, 1, 1));
+//         graph[2].add(new Edge(2, 3, 1));
+//         graph[2].add(new Edge(2, 4, 2));
+//         graph[3].add(new Edge(3, 1, 3));
+//         graph[3].add(new Edge(3, 2, 1));
+//     }
+//     public static void bfs(ArrayList<Edge>[] graph){
+//         Queue<Integer> q = new LinkedList<>();
+//         boolean vis[] = new boolean[graph.length];
+//         q.add(0);
+//         while(!q.isEmpty()){
+//             int curr = q.remove();
+//             if(!vis[curr]){
+//                 System.out.print(curr+" ");
+//                 vis[curr] = true;
+//                 for(int i=0;i<graph[curr].size();i++){
+//                     Edge e = graph[curr].get(i);
+//                     q.add(e.dest);
+//                 }
+//             }
+//         }
+//     }
+//     public static void main(String[] args){
+//         int V=5;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+//         bfs(graph);
+//     }
+// }
+
+// DFS : Depth First Search
 import java.util.*;
 public class Graphcodes {
     static class Edge{
@@ -62,19 +113,13 @@ public class Graphcodes {
         graph[3].add(new Edge(3, 1, 3));
         graph[3].add(new Edge(3, 2, 1));
     }
-    public static void bfs(ArrayList<Edge>[] graph){
-        Queue<Integer> q = new LinkedList<>();
-        boolean vis[] = new boolean[graph.length];
-        q.add(0);
-        while(!q.isEmpty()){
-            int curr = q.remove();
-            if(!vis[curr]){
-                System.out.print(curr+" ");
-                vis[curr] = true;
-                for(int i=0;i<graph[curr].size();i++){
-                    Edge e = graph[curr].get(i);
-                    q.add(e.dest);
-                }
+    public static void dfs(ArrayList<Edge>[] graph, int curr, boolean vis[]){
+        System.out.print(curr+" ");
+        vis[curr] = true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]){
+                dfs(graph, e.dest, vis);
             }
         }
     }
@@ -82,6 +127,7 @@ public class Graphcodes {
         int V=5;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
-        bfs(graph);
+        boolean vis[] = new boolean[V];
+        dfs(graph, 0, vis);
     }
 }
