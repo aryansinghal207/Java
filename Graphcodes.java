f767702f08b14ccee65f5211832e50009646ab8c
Aryan <aryansinghal207@gmail.com>
2025-10-16 12:09:33 +0530

    Graph code

--- Graphcodes.java
+++ Graphcodes.java
@@ -132,7 +132,56 @@
 //     }
 // }
 
-// Has_Path
+// // Has_Path
+// import java.util.*;
+// public class Graphcodes {
+//     static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s , int d , int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i=0;i<graph.length;i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1, 5));
+//         graph[1].add(new Edge(1, 0, 5));
+//         graph[1].add(new Edge(1, 2, 1));
+//         graph[1].add(new Edge(1, 3, 3));
+//         graph[2].add(new Edge(2, 1, 1));
+//         graph[2].add(new Edge(2, 3, 1));
+//         graph[2].add(new Edge(2, 4, 2));
+//         graph[3].add(new Edge(3, 1, 3));
+//         graph[3].add(new Edge(3, 2, 1));
+//     }
+//     public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean vis[]){
+//         if(src == dest){
+//             return true;
+//         }
+//         vis[src] = true;
+//         for(int i=0;i<graph[src].size();i++){
+//             Edge e = graph[src].get(i);
+//             if(!vis[e.dest] && hasPath(graph, e.dest, dest, vis)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public static void main(String[] args){
+//         int V=5;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+//         boolean vis[] = new boolean[V];
+//         System.out.println(hasPath(graph, 0, 4, vis));
+//     } 
+// } 
+
+// Connected Components
 import java.util.*;
 public class Graphcodes {
     static class Edge{
@@ -158,25 +207,39 @@ public class Graphcodes {
         graph[2].add(new Edge(2, 4, 2));
         graph[3].add(new Edge(3, 1, 3));
         graph[3].add(new Edge(3, 2, 1));
+
+        graph[4].add(new Edge(4, 5, 2));
+        graph[5].add(new Edge(5, 4, 2));
+        graph[5].add(new Edge(5, 6, 3));
+        graph[6].add(new Edge(6, 5, 3));
     }
-    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean vis[]){
-        if(src == dest){
-            return true;
-        }
-        vis[src] = true;
-        for(int i=0;i<graph[src].size();i++){
-            Edge e = graph[src].get(i);
-            if(!vis[e.dest] && hasPath(graph, e.dest, dest, vis)){
-                return true;
+    public static void dfs(ArrayList<Edge>[] graph){
+        boolean vis[] = new boolean[graph.length];
+        int count = 0;
+        for(int i=0;i<graph.length;i++){
+            if(!vis[i]){
+                System.out.print("Component "+count+" -> ");
+                dfsutil(graph, i, vis);
+                System.out.println();
+                count++;
             }
         }
-        return false;
+        System.out.println("Total Components : "+count);
+    }
+    public static void dfsutil(ArrayList<Edge>[] graph, int curr, boolean vis[]){
+        System.out.print(curr+" ");
+        vis[curr] = true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]){
+                dfsutil(graph, e.dest, vis);
+            }
+        }
     }
     public static void main(String[] args){
-        int V=5;
+        int V=7;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
-        boolean vis[] = new boolean[V];
-        System.out.println(hasPath(graph, 0, 4, vis));
+        dfs(graph);
     } 
-} 
+}
