c1866e2b901ff788d7390a5dd4b479700c767cfd
Aryan <aryansinghal207@gmail.com>
2025-10-16 15:17:52 +0530

    Graph code

--- Graphcodes.java
+++ Graphcodes.java
@@ -181,64 +181,120 @@
 //     } 
 // } 
 
-// Connected Components
+// // Connected Components
+// import java.util.*;
+// public class Graphcodes {
+//     static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s , int d , int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i=0;i<graph.length;i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1, 5));
+//         graph[1].add(new Edge(1, 0, 5));
+//         graph[1].add(new Edge(1, 2, 1));
+//         graph[1].add(new Edge(1, 3, 3));
+//         graph[2].add(new Edge(2, 1, 1));
+//         graph[2].add(new Edge(2, 3, 1));
+//         graph[2].add(new Edge(2, 4, 2));
+//         graph[3].add(new Edge(3, 1, 3));
+//         graph[3].add(new Edge(3, 2, 1));
+//         graph[4].add(new Edge(4, 5, 2));
+//         graph[5].add(new Edge(5, 4, 2));
+//         graph[5].add(new Edge(5, 6, 3));
+//         graph[6].add(new Edge(6, 5, 3));
+//     }
+//     public static void dfs(ArrayList<Edge>[] graph){
+//         boolean vis[] = new boolean[graph.length];
+//         int count = 0;
+//         for(int i=0;i<graph.length;i++){
+//             if(!vis[i]){
+//                 System.out.print("Component "+count+" -> ");
+//                 dfsutil(graph, i, vis);
+//                 System.out.println();
+//                 count++;
+//             }
+//         }
+//         System.out.println("Total Components : "+count);
+//     }
+//     public static void dfsutil(ArrayList<Edge>[] graph, int curr, boolean vis[]){
+//         System.out.print(curr+" ");
+//         vis[curr] = true;
+//         for(int i=0;i<graph[curr].size();i++){
+//             Edge e = graph[curr].get(i);
+//             if(!vis[e.dest]){
+//                 dfsutil(graph, e.dest, vis);
+//             }
+//         }
+//     }
+//     public static void main(String[] args){
+//         int V=7;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+//         dfs(graph);
+//     } 
+// }
+
+// Cycle Detection in Undirected Graph
 import java.util.*;
-public class Graphcodes {
-    static class Edge{
+public class Graphcodes{
+    public static class Edge{
         int src;
-        int dest;
-        int wt;
-        public Edge(int s , int d , int w){
+        int des;
+        public Edge(int s , int d){
             this.src = s;
-            this.dest = d;
-            this.wt = w;
+            this.des = d;
         }
     }
-    static void createGraph(ArrayList<Edge>[] graph){
+    public static void createGraph(ArrayList<Edge>[] graph){
         for(int i=0;i<graph.length;i++){
-            graph[i] = new ArrayList<>();
+            graph[i]=new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 5));
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 2));
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
-        graph[4].add(new Edge(4, 5, 2));
-        graph[5].add(new Edge(5, 4, 2));
-        graph[5].add(new Edge(5, 6, 3));
-        graph[6].add(new Edge(6, 5, 3));
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
     }
-    public static void dfs(ArrayList<Edge>[] graph){
-        boolean vis[] = new boolean[graph.length];
-        int count = 0;
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+        boolean vis[]=new boolean[graph.length];
         for(int i=0;i<graph.length;i++){
             if(!vis[i]){
-                System.out.print("Component "+count+" -> ");
-                dfsutil(graph, i, vis);
-                System.out.println();
-                count++;
+                if(isCycleUtil(graph, vis, -1, i)){
+                    return true;
+                }
             }
         }
-        System.out.println("Total Components : "+count);
+        return false;
     }
-    public static void dfsutil(ArrayList<Edge>[] graph, int curr, boolean vis[]){
-        System.out.print(curr+" ");
-        vis[curr] = true;
+    public static boolean isCycleUtil(ArrayList<Edge>[] graph, boolean vis[], int par, int curr){
+        vis[curr]=true;
         for(int i=0;i<graph[curr].size();i++){
-            Edge e = graph[curr].get(i);
-            if(!vis[e.dest]){
-                dfsutil(graph, e.dest, vis);
+            Edge e=graph[curr].get(i);
+            if(!vis[e.des]){
+                if(isCycleUtil(graph, vis, curr, e.des)){
+                    return true;
+                }
+            }
+            else if(par!=e.des){
+                return true;
             }
         }
+        return false; 
     }
     public static void main(String[] args){
-        int V=7;
-        ArrayList<Edge>[] graph = new ArrayList[V];
+        int V=3;
+        ArrayList<Edge>[] graph=new ArrayList[V];
         createGraph(graph);
-        dfs(graph);
-    } 
+        System.out.println(isCycle(graph));
+    }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,13 +1,2 @@
-// LinkedHashSet
-import java.util.*;
-public class Hashingcode{
-    public static void main(String[] args){
-        LinkedHashSet<String> set=new LinkedHashSet<>();
-        set.add("Delhi");
-        set.add("Pune");
-        set.add("Mumbai");
-        set.add("Odisha");
-        set.add("Banglore");
-        System.out.println(set);
-    }
-}
+
+        graph[2].add(new Edge(2, 0));
