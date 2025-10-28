d40478681de3a672f49a638811e899138e9a4715
Aryan <aryansinghal207@gmail.com>
2025-10-28 15:20:38 +0530

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -66,75 +66,53 @@
 // }
 
 
-
-// // WAP to find subset of a string
+// // N-Queen
 // import java.util.*;
-// public class Backtracking {
-//     public static void generateSubsets(String str, int index, String current) {
-//         if (index == str.length()) {
-//             System.out.println(current);
+// public class Backtracking{
+//     public static boolean issafe(char board[][], int row, int col){
+//         for (int i = row - 1; i >= 0; i--) {
+//             if (board[i][col] == 'Q') return false;
+//         }
+//         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+//             if (board[i][j] == 'Q') return false;
+//         }
+//         for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+//             if (board[i][j] == 'Q') return false;
+//         }
+//         return true;
+//     }
+//     public static void nqueen(char board[][],int row){
+//         if(row==board.length){
+//             printboard(board);
 //             return;
 //         }
-//         generateSubsets(str, index + 1, current + str.charAt(index));
-//         generateSubsets(str, index + 1, current);
+//         for(int j=0;j<board.length;j++){
+//             if(issafe(board,row,j)){
+//                 board[row][j]='Q';
+//                 nqueen(board,row+1);
+//                 board[row][j]='.';
+//             }
+//         }
 //     }
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("Enter a string: ");
-//         String input = sc.nextLine();
-
-//         System.out.println("Subsets of the string are:");
-//         generateSubsets(input, 0, "");
+//     public static void printboard(char board[][]){
+//         System.out.println("----- Chess Board -----");
+//         for(int i=0;i<board.length;i++){
+//             for(int j=0;j<board.length;j++){
+//                 System.out.print(board[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args){
+//         int n=4;
+//         char board[][]=new char[n][n];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 board[i][j]='.';
+//             }
+//         }
+//         nqueen(board,0);
 //     }
 // }
 
-
-// N-Queen
-import java.util.*;
-public class Backtracking{
-    public static boolean issafe(char board[][], int row, int col){
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') return false;
-        }
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
-        }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') return false;
-        }
-        return true;
-    }
-    public static void nqueen(char board[][],int row){
-        if(row==board.length){
-            printboard(board);
-            return;
-        }
-        for(int j=0;j<board.length;j++){
-            if(issafe(board,row,j)){
-                board[row][j]='Q';
-                nqueen(board,row+1);
-                board[row][j]='.';
-            }
-        }
-    }
-    public static void printboard(char board[][]){
-        System.out.println("----- Chess Board -----");
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-    public static void main(String[] args){
-        int n=4;
-        char board[][]=new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                board[i][j]='.';
-            }
-        }
-        nqueen(board,0);
-    }
-}
