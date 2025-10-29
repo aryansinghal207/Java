952e74f370ed4f638bdf7b5c97a5cf6e20ae5cd3
Aryan <aryansinghal207@gmail.com>
2025-10-29 14:02:23 +0530

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -169,57 +169,74 @@
 // }
 
 
-// Print only one possible soltuion of N-Queen.
+// // Print only one possible soltuion of N-Queen.
+// import java.util.*;
+// public class Backtracking{
+//     public static boolean issafe(char[][] board,int row,int col){
+//         for(int i=row-1;i>=0;i--){
+//             if(board[i][col]=='Q') return false;
+//         }
+//         for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+//             if(board[i][j]=='Q') return false;
+//         }
+//         for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
+//             if(board[i][j]=='Q') return false;
+//         }
+//         return true;
+//     }
+//     public static boolean nqueen(char[][] board,int row){
+//         if(row==board.length){
+//             return true;
+//         }
+//         for(int j=0;j<board.length;j++){
+//             if(issafe(board,row,j)){
+//                 board[row][j]='Q';
+//                 if(nqueen(board,row+1)) return true;
+//                 board[row][j]='.';
+//             }
+//         }
+//         return false;
+//     }
+//     public static void printboard(char[][] board){
+//         System.out.println("----- Chess Board -----");
+//         for(int i=0;i<board.length;i++){
+//             for(int j=0;j<board.length;j++){
+//                 System.out.print(board[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args){
+//         int n=4;
+//         char board[][]=new char[n][n];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 board[i][j]='.';
+//             }
+//         }
+//         if(nqueen(board,0)){
+//             System.out.println("Solution is possible");
+//             printboard(board);
+//         }else{
+//             System.out.println("Solution is not possible.");
+//         }
+//     }
+// }
+
+
+// Grid Codes ways
 import java.util.*;
 public class Backtracking{
-    public static boolean issafe(char[][] board,int row,int col){
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q') return false;
-        }
-        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-            if(board[i][j]=='Q') return false;
-        }
-        for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
-            if(board[i][j]=='Q') return false;
-        }
-        return true;
-    }
-    public static boolean nqueen(char[][] board,int row){
-        if(row==board.length){
-            return true;
-        }
-        for(int j=0;j<board.length;j++){
-            if(issafe(board,row,j)){
-                board[row][j]='Q';
-                if(nqueen(board,row+1)) return true;
-                board[row][j]='.';
-            }
-        }
-        return false;
-    }
-    public static void printboard(char[][] board){
-        System.out.println("----- Chess Board -----");
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    public static int gridways(int i,int j,int n,int m){
+        if(i==n-1 && j==m-1) return 1;
+        else if(i==n || j==n) return 0;
+        int w1=gridways(i+1,j,n,m);
+        int w2=gridways(i,j+1,n,m);
+        return w1+w2;
     }
     public static void main(String[] args){
-        int n=4;
-        char board[][]=new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                board[i][j]='.';
-            }
-        }
-        if(nqueen(board,0)){
-            System.out.println("Solution is possible");
-            printboard(board);
-        }else{
-            System.out.println("Solution is not possible.");
-        }
+        int n=3,m=3;
+        System.out.println(gridways(0,0,n,m));
     }
 }
