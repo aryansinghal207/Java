a81dd5ea85941e17b019817c21a5862bc6f4cc56
Aryan <aryansinghal207@gmail.com>
2025-10-30 15:40:24 +0530

    BinaryTree

--- BinaryTreecode.java
+++ BinaryTreecode.java
@@ -587,10 +587,18 @@ public class BinaryTreecode{
             }
         }
     }
+    public static int height(Node root){
+            if(root==null) return 0;
+            int lh=height(root.left);
+            int rh=height(root.right);
+            return Math.max(lh,rh)+1;
+        }
     public static void main(String[] args){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root=tree.buildtree(nodes);
         tree.levelOrder(root);
+        System.out.println();
+        System.out.println("Height of tree : "+height(root));
     }
 }
