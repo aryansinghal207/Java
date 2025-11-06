8910b39da7408b6acea37d78e0c60b87a803d825
Aryan <aryansinghal207@gmail.com>
2025-11-06 12:06:18 +0530

    Array.java Changes

--- Array.java
+++ Array.java
@@ -560,7 +560,6 @@
 //         int prefix=0;
 //         for(int i=0;i<arr.length;i++){
 //             prefix+=arr[i];
-
 //             int suffixsum=total-prefix;
 //             if(prefix==suffixsum){
 //                 return true;
