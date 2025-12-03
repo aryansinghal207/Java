c2df1804377a4c4ad745faa6cbd47dfcae5ce4a4
Aryan <aryansinghal207@gmail.com>
2025-12-03 20:21:04 +0530

    Updated

--- Graphcodes.java
+++ Graphcodes.java
@@ -385,86 +385,89 @@
 //     }
 // }
 
-// Bipartite and Cycle in Directed Graph
-import java.util.*;
-public class Graphcodes{
-    public static class Edge{
-        int src;
-        int des;
-        public Edge(int s,int d){
-            this.src=s;
-            this.des=d;
-        }
-    }
-    public static void createGraph(ArrayList<Edge>[] graph){
-        for(int i=0;i<graph.length;i++){
-            graph[i]=new ArrayList<>();
-        }
-        // graph[0].add(new Edge(0,1));
-        // graph[0].add(new Edge(0,2));
-        // graph[1].add(new Edge(1,0));
-        // graph[1].add(new Edge(1,3));
-        // graph[2].add(new Edge(2,0));
-        // graph[2].add(new Edge(2,4));
-        // graph[3].add(new Edge(3,1));
-        // // graph[3].add(new Edge(3,4));
-        // graph[4].add(new Edge(4,2));
-        // // graph[4].add(new Edge(4,3));
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
-        graph[1].add(new Edge(1, 3));
-        graph[2].add(new Edge(2, 4));
-    }
-    public static boolean isBipartite(ArrayList<Edge>[] graph){
-        int col[]=new int[graph.length];
-        for(int i=0;i<graph.length;i++){
-            col[i]=-1;
-        }
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<graph.length;i++){
-            if(col[i]==-1){
-                q.add(i);
-                col[i]=0;
-                while(!q.isEmpty()){
-                    int curr=q.remove();
-                    for(int j=0;j<graph[curr].size();j++){
-                        Edge e=graph[curr].get(j);
-                        if(col[e.des]==-1){
-                            int next=col[curr]==0?1:0;
-                            col[e.des]=next;
-                            q.add(e.des);
-                        }else if(col[e.des]==col[curr]) return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    public static boolean isCycle(ArrayList<Edge>[] graph){
-        boolean[] vis=new boolean[graph.length];
-        boolean[] st=new boolean[graph.length];
-        for(int i=0;i<graph.length;i++){
-            if(!vis[i]){
-                if(isCycleUtil(graph,i,vis,st)) return true;
-            }
-        }
-        return false;
-    }
-    public static boolean isCycleUtil(ArrayList<Edge>[] graph,int curr,boolean[] vis,boolean[] st){
-        st[curr]=true;vis[curr]=true;
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e=graph[curr].get(i);
-            if(st[e.des]) return true;
-            if(!vis[e.des] && isCycleUtil(graph,e.des,vis,st)) return true;
-        }
-        st[curr]=false;
-        return false;
-    }
-    public static void main(String[] args){
-        int V=5;
-        ArrayList<Edge>[] graph=new ArrayList[V];
-        createGraph(graph);
-        System.out.println(isCycle(graph));
-        System.out.print(isBipartite(graph));
-    }
-}
+// // Bipartite and Cycle in Directed Graph
+// import java.util.*;
+// public class Graphcodes{
+//     public static class Edge{
+//         int src;
+//         int des;
+//         public Edge(int s,int d){
+//             this.src=s;
+//             this.des=d;
+//         }
+//     }
+//     public static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i=0;i<graph.length;i++){
+//             graph[i]=new ArrayList<>();
+//         }
+//         // graph[0].add(new Edge(0,1));
+//         // graph[0].add(new Edge(0,2));
+//         // graph[1].add(new Edge(1,0));
+//         // graph[1].add(new Edge(1,3));
+//         // graph[2].add(new Edge(2,0));
+//         // graph[2].add(new Edge(2,4));
+//         // graph[3].add(new Edge(3,1));
+//         // // graph[3].add(new Edge(3,4));
+//         // graph[4].add(new Edge(4,2));
+//         // // graph[4].add(new Edge(4,3));
+//         graph[0].add(new Edge(0, 1));
+//         graph[0].add(new Edge(0, 2));
+//         graph[1].add(new Edge(1, 3));
+//         graph[2].add(new Edge(2, 4));
+//     }
+//     public static boolean isBipartite(ArrayList<Edge>[] graph){
+//         int col[]=new int[graph.length];
+//         for(int i=0;i<graph.length;i++){
+//             col[i]=-1;
+//         }
+//         Queue<Integer> q=new LinkedList<>();
+//         for(int i=0;i<graph.length;i++){
+//             if(col[i]==-1){
+//                 q.add(i);
+//                 col[i]=0;
+//                 while(!q.isEmpty()){
+//                     int curr=q.remove();
+//                     for(int j=0;j<graph[curr].size();j++){
+//                         Edge e=graph[curr].get(j);
+//                         if(col[e.des]==-1){
+//                             int next=col[curr]==0?1:0;
+//                             col[e.des]=next;
+//                             q.add(e.des);
+//                         }else if(col[e.des]==col[curr]) return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+//     public static boolean isCycle(ArrayList<Edge>[] graph){
+//         boolean[] vis=new boolean[graph.length];
+//         boolean[] st=new boolean[graph.length];
+//         for(int i=0;i<graph.length;i++){
+//             if(!vis[i]){
+//                 if(isCycleUtil(graph,i,vis,st)) return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public static boolean isCycleUtil(ArrayList<Edge>[] graph,int curr,boolean[] vis,boolean[] st){
+//         st[curr]=true;vis[curr]=true;
+//         for(int i=0;i<graph[curr].size();i++){
+//             Edge e=graph[curr].get(i);
+//             if(st[e.des]) return true;
+//             if(!vis[e.des] && isCycleUtil(graph,e.des,vis,st)) return true;
+//         }
+//         st[curr]=false;
+//         return false;
+//     }
+//     public static void main(String[] args){
+//         int V=5;
+//         ArrayList<Edge>[] graph=new ArrayList[V];
+//         createGraph(graph);
+//         System.out.println(isCycle(graph));
+//         System.out.print(isBipartite(graph));
+//     }
+// }
+
+
+// Dijkstra's Algorithm
