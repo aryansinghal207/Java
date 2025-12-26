242e0afc708dca5f783c5548ac91d4e874e31750
Aryan <aryansinghal207@gmail.com>
2025-12-26 22:23:07 +0530

    updates

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,5 +1,16 @@
-
-        int[] result = dijkstra(V, adj, S);
-        System.out.println("Shortest distances from node " + S + " :");
-        for (int dist : result) {
-            System.out.print(dist + " ");
+import java.util.*;
+public class Recursion{
+    static void print(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        print(n-1);
+        System.out.println(n);   
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        print(n);
+    }
+}
